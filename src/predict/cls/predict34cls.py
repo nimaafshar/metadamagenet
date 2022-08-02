30ec83a5bcd06e50b129361b2efd9357814a1e0c
nimaafshar <nima.afsharh@gmail.com>
2022-08-02 19:32:32 +0430

    refactoring predict process

--- src/predict/cls/predict34cls.py
+++ src/predict/cls/predict34cls.py
@@ -1,6 +1,9 @@
 from os import path, makedirs, listdir
 import sys
+
+import emoji
 import numpy as np
+import numpy.typing as npt
 import torch
 from torch import nn
 from torch.autograd import Variable
@@ -9,10 +12,12 @@ from tqdm import tqdm
 import timeit
 import cv2
 
-from src.configs import MODELS_WEIGHTS_FOLDER
+from src.configs import MODELS_WEIGHTS_FOLDER, TEST_DIR
 from src.setup import set_random_seeds
 from src.util.utils import normalize_colors
 from src.zoo.models import Res34_Unet_Double
+from src.file_structure import Dataset, ImageData, DataTime
+from src.logs import log
 
 set_random_seeds()
 cv2.setNumThreads(0)
@@ -30,8 +35,6 @@ def predict(seed: int):
 
     # cudnn.benchmark = True
 
-    models = []
-
     snap_to_load = 'res34_cls2_{}_tuned_best'.format(seed)
     model = Res34_Unet_Double().cuda()
     model = nn.DataParallel(model).cuda()
@@ -47,47 +50,44 @@ def predict(seed: int):
     print("loaded checkpoint '{}' (epoch {}, best_score {})"
           .format(snap_to_load, checkpoint['epoch'], checkpoint['best_score']))
     model.eval()
-    models.append(model)
+
+    test_dataset = Dataset((TEST_DIR,))
+
+    log(f":mag: discovering {TEST_DIR.absolute()}...")
+    test_dataset.discover()
+    log(f":file_folder: {len(test_dataset)} files found")
 
     with torch.no_grad():
-        for f in tqdm(sorted(listdir(test_dir))):
-            if '_pre_' in f:
-                fn = path.join(test_dir, f)
+        image_data: ImageData
+        for image_data in tqdm(test_dataset.images):
+            pre_image: npt.NDArray = cv2.imread(image_data.image(DataTime.PRE), cv2.IMREAD_COLOR)
+            post_image: npt.NDArray = cv2.imread(image_data.image(DataTime.POST), cv2.IMREAD_COLOR)
 
-                img = cv2.imread(fn, cv2.IMREAD_COLOR)
-                img2 = cv2.imread(fn.replace('_pre_', '_post_'), cv2.IMREAD_COLOR)
+            img = np.concatenate((pre_image, post_image), axis=2)
+            img = normalize_colors(img)
 
-                img = np.concatenate([img, img2], axis=2)
-                img = normalize_colors(img)
+            inp = np.asarray((img,
+                              img[::-1, ...],
+                              img[:, ::-1, ...],
+                              img[::-1, ::-1, ...]), dtype='float')
+            inp = torch.from_numpy(inp.transpose((0, 3, 1, 2))).float()
+            inp = inp.cuda()
 
-                inp = []
-                inp.append(img)
-                inp.append(img[::-1, ...])
-                inp.append(img[:, ::-1, ...])
-                inp.append(img[::-1, ::-1, ...])
-                inp = np.asarray(inp, dtype='float')
-                inp = torch.from_numpy(inp.transpose((0, 3, 1, 2))).float()
-                inp = Variable(inp).cuda()
+            msk = model(inp)
+            msk = torch.sigmoid(msk)
+            msk = msk.cpu().numpy()
 
-                pred = []
-                for model in models:
-                    msk = model(inp)
-                    msk = torch.sigmoid(msk)
-                    msk = msk.cpu().numpy()
+            pred = np.asarray((msk[0, ...],
+                               msk[1, :, ::-1, :],
+                               msk[2, :, :, ::-1],
+                               msk[3, :, ::-1, ::-1])).mean(axis=0)
 
-                    pred.append(msk[0, ...])
-                    pred.append(msk[1, :, ::-1, :])
-                    pred.append(msk[2, :, :, ::-1])
-                    pred.append(msk[3, :, ::-1, ::-1])
-
-                pred_full = np.asarray(pred).mean(axis=0)
-
-                msk = pred_full * 255
-                msk = msk.astype('uint8').transpose(1, 2, 0)
-                cv2.imwrite(path.join(pred_folder, '{0}.png'.format(f.replace('.png', '_part1.png'))), msk[..., :3],
-                            [cv2.IMWRITE_PNG_COMPRESSION, 9])
-                cv2.imwrite(path.join(pred_folder, '{0}.png'.format(f.replace('.png', '_part2.png'))), msk[..., 2:],
-                            [cv2.IMWRITE_PNG_COMPRESSION, 9])
+            msk = pred * 255
+            msk = msk.astype('uint8').transpose(1, 2, 0)
+            cv2.imwrite(path.join(pred_folder, '{0}.png'.format(f.replace('.png', '_part1.png'))), msk[..., :3],
+                        [cv2.IMWRITE_PNG_COMPRESSION, 9])
+            cv2.imwrite(path.join(pred_folder, '{0}.png'.format(f.replace('.png', '_part2.png'))), msk[..., 2:],
+                        [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
 
 if __name__ == '__main__':
