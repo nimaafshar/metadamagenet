f1ff7c8bbb0a9a730b97df606edfe5b6fc334522
nimaafshar <nima.afsharh@gmail.com>
2022-08-05 14:40:48 +0430

    minor refactor

--- src/train/loc/train34_loc.py
+++ src/train/loc/train34_loc.py
@@ -1,13 +1,11 @@
 import os
-os.environ["MKL_NUM_THREADS"] = "2" 
-os.environ["NUMEXPR_NUM_THREADS"] = "2" 
-os.environ["OMP_NUM_THREADS"] = "2" 
-
 from os import path, makedirs, listdir
 import sys
 import numpy as np
+
 np.random.seed(1)
 import random
+
 random.seed(1)
 
 import torch
@@ -17,28 +15,39 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import torch.optim.lr_scheduler as lr_scheduler
 
-from adamw import AdamW
-from losses import dice_round, ComboLoss
+from src.optim.adamw import AdamW
+from src.losses import dice_round, ComboLoss
 
-import pandas as pd
 from tqdm import tqdm
 import timeit
 import cv2
 
-from zoo.models import Res34_Unet_Loc
+from src.zoo.models import Res34_Unet_Loc
 
 from imgaug import augmenters as iaa
 
-from utils import *
+from src.util.augmentations import (
+    shift_image,
+    rotate_image,
+    shift_channels,
+    change_hsv,
+    clahe,
+    gauss_noise,
+    blur,
+    saturation,
+    brightness,
+    contrast
+)
+from src.util.utils import normalize_colors
 
 from sklearn.model_selection import train_test_split
 
-from sklearn.metrics import accuracy_score
-
-import gc
-
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
+
+os.environ["MKL_NUM_THREADS"] = "2"
+os.environ["NUMEXPR_NUM_THREADS"] = "2"
+os.environ["OMP_NUM_THREADS"] = "2"
 
 train_dirs = ['train', 'tier3']
 
@@ -46,6 +55,7 @@ models_folder = 'weights'
 
 input_shape = (736, 736)
 
+# load all pre_disaster images
 
 all_files = []
 for d in train_dirs:
@@ -58,7 +68,7 @@ class TrainData(Dataset):
     def __init__(self, train_idxs):
         super().__init__()
         self.train_idxs = train_idxs
-        self.elastic = iaa.ElasticTransformation(alpha=(0.25, 1.2), sigma=0.2)
+        self.elastic: iaa.ElasticTransformation = iaa.ElasticTransformation(alpha=(0.25, 1.2), sigma=0.2)
 
     def __len__(self):
         return len(self.train_idxs)
@@ -68,74 +78,91 @@ class TrainData(Dataset):
 
         fn = all_files[_idx]
 
+        # read pre_disaster image
         img = cv2.imread(fn, cv2.IMREAD_COLOR)
-        
+
         if random.random() > 0.985:
+            # read post_disaster image
             img = cv2.imread(fn.replace('_pre_disaster', '_post_disaster'), cv2.IMREAD_COLOR)
 
+        # read pre_disaster mask
         msk0 = cv2.imread(fn.replace('/images/', '/masks/'), cv2.IMREAD_UNCHANGED)
 
         if random.random() > 0.5:
+            # top-down flip
             img = img[::-1, ...]
             msk0 = msk0[::-1, ...]
 
         if random.random() > 0.05:
+            # rotate 0 or 90 or 180 or 270 degrees
             rot = random.randrange(4)
             if rot > 0:
                 img = np.rot90(img, k=rot)
                 msk0 = np.rot90(msk0, k=rot)
 
         if random.random() > 0.8:
+            # shift image
             shift_pnt = (random.randint(-320, 320), random.randint(-320, 320))
             img = shift_image(img, shift_pnt)
             msk0 = shift_image(msk0, shift_pnt)
-            
+
         if random.random() > 0.2:
-            rot_pnt =  (img.shape[0] // 2 + random.randint(-320, 320), img.shape[1] // 2 + random.randint(-320, 320))
+            # rotate by point, scale
+            rot_pnt = (img.shape[0] // 2 + random.randint(-320, 320), img.shape[1] // 2 + random.randint(-320, 320))
             scale = 0.9 + random.random() * 0.2
             angle = random.randint(0, 20) - 10
             if (angle != 0) or (scale != 1):
                 img = rotate_image(img, angle, scale, rot_pnt)
                 msk0 = rotate_image(msk0, angle, scale, rot_pnt)
 
-        crop_size = input_shape[0]
         if random.random() > 0.3:
+            # crop
             crop_size = random.randint(int(input_shape[0] / 1.2), int(input_shape[0] / 0.8))
 
-        bst_x0 = random.randint(0, img.shape[1] - crop_size)
-        bst_y0 = random.randint(0, img.shape[0] - crop_size)
-        bst_sc = -1
-        try_cnt = random.randint(1, 5)
-        for i in range(try_cnt):
-            x0 = random.randint(0, img.shape[1] - crop_size)
-            y0 = random.randint(0, img.shape[0] - crop_size)
-            _sc = msk0[y0:y0+crop_size, x0:x0+crop_size].sum()
-            if _sc > bst_sc:
-                bst_sc = _sc
-                bst_x0 = x0
-                bst_y0 = y0
-        x0 = bst_x0
-        y0 = bst_y0
-        img = img[y0:y0+crop_size, x0:x0+crop_size, :]
-        msk0 = msk0[y0:y0+crop_size, x0:x0+crop_size]
+            bst_x0 = random.randint(0, img.shape[1] - crop_size)
+            bst_y0 = random.randint(0, img.shape[0] - crop_size)
+            bst_sc = -1
+            try_cnt = random.randint(1, 5)
+            for i in range(try_cnt):
+                x0 = random.randint(0, img.shape[1] - crop_size)
+                y0 = random.randint(0, img.shape[0] - crop_size)
+                _sc = msk0[y0:y0 + crop_size, x0:x0 + crop_size].sum()
+                if _sc > bst_sc:
+                    bst_sc = _sc
+                    bst_x0 = x0
+                    bst_y0 = y0
+            x0 = bst_x0
+            y0 = bst_y0
+            img = img[y0:y0 + crop_size, x0:x0 + crop_size, :]
+            msk0 = msk0[y0:y0 + crop_size, x0:x0 + crop_size]
 
-        
-        if crop_size != input_shape[0]:
-            img = cv2.resize(img, input_shape, interpolation=cv2.INTER_LINEAR)
-            msk0 = cv2.resize(msk0, input_shape, interpolation=cv2.INTER_LINEAR)
+            if crop_size != input_shape[0]:
+                img = cv2.resize(img, input_shape, interpolation=cv2.INTER_LINEAR)
+                msk0 = cv2.resize(msk0, input_shape, interpolation=cv2.INTER_LINEAR)
+
+        # TODO: check if input shape equals img.shape
 
         if random.random() > 0.97:
-            img = shift_channels(img, random.randint(-5, 5), random.randint(-5, 5), random.randint(-5, 5))
+            img = shift_channels(img,
+                                 random.randint(-5, 5),
+                                 random.randint(-5, 5),
+                                 random.randint(-5, 5))
+
         elif random.random() > 0.97:
-            img = change_hsv(img, random.randint(-5, 5), random.randint(-5, 5), random.randint(-5, 5))
+            img = change_hsv(img,
+                             random.randint(-5, 5),
+                             random.randint(-5, 5),
+                             random.randint(-5, 5))
 
         if random.random() > 0.93:
+
             if random.random() > 0.97:
                 img = clahe(img)
             elif random.random() > 0.97:
                 img = gauss_noise(img)
             elif random.random() > 0.97:
-                img = cv2.blur(img, (3, 3))
+                img = blur(img)
+
         elif random.random() > 0.93:
             if random.random() > 0.97:
                 img = saturation(img, 0.9 + random.random() * 0.2)
@@ -143,8 +170,9 @@ class TrainData(Dataset):
                 img = brightness(img, 0.9 + random.random() * 0.2)
             elif random.random() > 0.97:
                 img = contrast(img, 0.9 + random.random() * 0.2)
-                
+
         if random.random() > 0.97:
+            # apply elastic transformation
             el_det = self.elastic.to_deterministic()
             img = el_det.augment_image(img)
 
@@ -152,7 +180,7 @@ class TrainData(Dataset):
 
         msk = (msk > 127) * 1
 
-        img = preprocess_inputs(img)
+        img = normalize_colors(img)
 
         img = torch.from_numpy(img.transpose((2, 0, 1))).float()
         msk = torch.from_numpy(msk.transpose((2, 0, 1))).long()
@@ -182,7 +210,7 @@ class ValData(Dataset):
 
         msk = (msk > 127) * 1
 
-        img = preprocess_inputs(img)
+        img = normalize_colors(img)
 
         img = torch.from_numpy(img.transpose((2, 0, 1))).float()
         msk = torch.from_numpy(msk.transpose((2, 0, 1))).long()
@@ -200,11 +228,11 @@ def validate(net, data_loader):
         for i, sample in enumerate(tqdm(data_loader)):
             msks = sample["msk"].numpy()
             imgs = sample["img"].cuda(non_blocking=True)
-            
+
             out = model(imgs)
 
             msk_pred = torch.sigmoid(out[:, 0, ...]).cpu().numpy()
-            
+
             for j in range(msks.shape[0]):
                 dices0.append(dice(msks[j, 0], msk_pred[j] > _thr))
 
@@ -230,7 +258,6 @@ def evaluate_val(data_val, best_score, model, snapshot_name, current_epoch):
     return best_score
 
 
-
 def train_epoch(current_epoch, seg_loss, model, optimizer, scheduler, train_data_loader):
     losses = AverageMeter()
 
@@ -241,7 +268,7 @@ def train_epoch(current_epoch, seg_loss, model, optimizer, scheduler, train_data
     for i, sample in enumerate(iterator):
         imgs = sample["img"].cuda(non_blocking=True)
         msks = sample["msk"].cuda(non_blocking=True)
-        
+
         out = model(imgs)
 
         loss = seg_loss(out, msks)
@@ -257,7 +284,7 @@ def train_epoch(current_epoch, seg_loss, model, optimizer, scheduler, train_data
         iterator.set_description(
             "epoch: {}; lr {:.7f}; Loss {loss.val:.4f} ({loss.avg:.4f}); Dice {dice.val:.4f} ({dice.avg:.4f})".format(
                 current_epoch, scheduler.get_lr()[-1], loss=losses, dice=dices))
-        
+
         optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.999)
@@ -266,15 +293,14 @@ def train_epoch(current_epoch, seg_loss, model, optimizer, scheduler, train_data
     scheduler.step(current_epoch)
 
     print("epoch: {}; lr {:.7f}; Loss {loss.avg:.4f}; Dice {dice.avg:.4f}".format(
-                current_epoch, scheduler.get_lr()[-1], loss=losses, dice=dices))
-
+        current_epoch, scheduler.get_lr()[-1], loss=losses, dice=dices))
 
 
 if __name__ == '__main__':
     t0 = timeit.default_timer()
 
     makedirs(models_folder, exist_ok=True)
-    
+
     seed = int(sys.argv[1])
     # vis_dev = sys.argv[2]
 
@@ -301,7 +327,8 @@ if __name__ == '__main__':
     data_train = TrainData(train_idxs)
     val_train = ValData(val_idxs)
 
-    train_data_loader = DataLoader(data_train, batch_size=batch_size, num_workers=6, shuffle=True, pin_memory=False, drop_last=True)
+    train_data_loader = DataLoader(data_train, batch_size=batch_size, num_workers=6, shuffle=True, pin_memory=False,
+                                   drop_last=True)
     val_data_loader = DataLoader(val_train, batch_size=val_batch_size, num_workers=6, shuffle=False, pin_memory=False)
 
     model = Res34_Unet_Loc()
@@ -310,11 +337,13 @@ if __name__ == '__main__':
 
     optimizer = AdamW(params, lr=0.00015, weight_decay=1e-6)
 
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[5, 11, 17, 25, 33, 47, 50, 60, 70, 90, 110, 130, 150, 170, 180, 190], gamma=0.5)
+    scheduler = lr_scheduler.MultiStepLR(optimizer,
+                                         milestones=[5, 11, 17, 25, 33, 47, 50, 60, 70, 90, 110, 130, 150, 170, 180,
+                                                     190], gamma=0.5)
 
     model = nn.DataParallel(model).cuda()
 
-    seg_loss = ComboLoss({'dice': 1.0, 'focal': 10.0}, per_image=False).cuda() #True
+    seg_loss = ComboLoss({'dice': 1.0, 'focal': 10.0}, per_image=False).cuda()  # True
 
     best_score = 0
     _cnt = -1
