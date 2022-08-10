789b8c22e9432099f5915748e8ff18b7a12310c2
nimaafshar <nima.afsharh@gmail.com>
2022-08-11 01:34:14 +0430

    training of Resnet34UnetDouble integrated with trainer api

--- examples/train/cls/resnet34_unet_double.py
+++ examples/train/cls/resnet34_unet_double.py
@@ -1,9 +1,27 @@
 import sys
 import timeit
+import random
+import os
+
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torch import nn
+from torch.optim.optimizer import Optimizer
+from torch.optim.lr_scheduler import MultiStepLR
+from apex import amp
 
 from src.train.dataset import ClassificationDataset, ClassificationValidationDataset
 from src.file_structure import Dataset as ImageDataset
 from src import configs
+from src.optim import AdamW
+from src.train.cls import ClassificationTrainer, ClassificationRequirements
+from src.train.trainer import TrainingConfig
+from src.model_config import ModelConfig
+from src.zoo.models import Res34_Unet_Double, Res34_Unet_Loc
+from src.losses import ComboLoss
+from src.setup import set_random_seeds
 
 from src.augment import (
     OneOf,
@@ -25,6 +43,72 @@ from src.augment import (
     Contrast
 )
 
+
+os.environ["MKL_NUM_THREADS"] = "2"
+os.environ["NUMEXPR_NUM_THREADS"] = "2"
+os.environ["OMP_NUM_THREADS"] = "2"
+
+set_random_seeds()
+
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+
+
+class Resnet34UnetDoubleTrainer(ClassificationTrainer):
+
+    def _setup(self):
+        super(Resnet34UnetDoubleTrainer, self)._setup()
+        np.random.seed(self._config.model_config.seed + 321)
+        random.seed(self._config.model_config.seed + 321)
+
+    def _get_dataloaders(self) -> (DataLoader, DataLoader):
+        return (DataLoader(self._config.train_dataset,
+                           batch_size=self._config.batch_size,
+                           num_workers=6,
+                           shuffle=True,
+                           pin_memory=False,
+                           drop_last=True),
+
+                DataLoader(self._config.validation_dataset,
+                           batch_size=self._config.val_batch_size,
+                           num_workers=6,
+                           shuffle=False,
+                           pin_memory=False))
+
+    def _get_requirements(self) -> ClassificationRequirements:
+        model: nn.Module = self._get_model()
+
+        optimizer: Optimizer = AdamW(model.parameters(),
+                                     lr=0.0002,
+                                     weight_decay=1e-6)
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
+
+        lr_scheduler: MultiStepLR = MultiStepLR(optimizer,
+                                                milestones=[5, 11, 17, 23, 29, 33, 47, 50, 60, 70, 90, 110, 130, 150,
+                                                            170, 180, 190],
+                                                gamma=0.5)
+        model: nn.Module = nn.DataParallel(model).cuda()
+
+        seg_loss: ComboLoss = ComboLoss({'dice': 1.0, 'focal': 12.0}, per_image=False).cuda()
+        ce_loss: nn.CrossEntropyLoss = nn.CrossEntropyLoss().cuda()
+        return ClassificationRequirements(
+            model,
+            optimizer,
+            lr_scheduler,
+            seg_loss,
+            ce_loss,
+            label_loss_weights=np.array([0.05, 0.2, 0.8, 0.7, 0.4])
+        )
+
+    def _update_weights(self, loss: torch.Tensor) -> None:
+        self._optimizer.zero_grad()
+        with amp.scale_loss(loss, self._optimizer) as scaled_loss:
+            scaled_loss.backward()
+        torch.nn.utils.clip_grad_norm_(amp.master_params(self._optimizer), 0.999)
+        self._optimizer.step()
+
+
 if __name__ == '__main__':
     t0 = timeit.default_timer()
 
@@ -32,6 +116,7 @@ if __name__ == '__main__':
 
     input_shape = (608, 608)
 
+    # TODO: count images with 2,3 damage level 2 times
     train_image_dataset = ImageDataset((configs.TRAIN_SPLIT,))
     train_image_dataset.discover()
 
@@ -185,4 +270,28 @@ if __name__ == '__main__':
 
     validation_dataset = ClassificationValidationDataset(
         image_dataset=valid_image_data,
+    )
+
+    model_config: ModelConfig = ModelConfig(
+        name='res34_cls2',
+        model_type=Res34_Unet_Double,
+        version='0',
+        seed=seed
+    )
+
+    training_config: TrainingConfig = TrainingConfig(
+        model_config=model_config,
+        input_shape=input_shape,
+        epochs=20,
+        batch_size=16,
+        val_batch_size=8,
+        train_dataset=train_dataset,
+        validation_dataset=validation_dataset,
+        evaluation_interval=2,
+        start_checkpoint=ModelConfig(
+            name='res34_loc',
+            model_type=Res34_Unet_Loc,
+            version='1',
+            seed=seed,
+        ),
     )
