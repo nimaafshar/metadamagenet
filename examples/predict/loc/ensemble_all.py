dabe25a1871e66aaafdf77440755214919212146
nimaafshar <nima.afsharh@gmail.com>
2022-08-19 20:18:53 +0430

    ensemble the results of all models

---
+++ examples/predict/loc/ensemble_all.py
@@ -0,0 +1,60 @@
+import timeit
+
+import cv2
+
+from src.configs import GeneralConfig
+from src.zoo.models import SeResNext50_Unet_Loc, Dpn92_Unet_Loc, SeNet154_Unet_Loc, Res34_Unet_Loc
+from src.setup import set_random_seeds
+from src.model_config import ModelConfig
+from src.file_structure import Dataset
+from src.predict.loc import LocalizationPredictor
+from src.logs import log
+
+set_random_seeds()
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+
+if __name__ == '__main__':
+    t0 = timeit.default_timer()
+
+    GeneralConfig.load()
+    config = GeneralConfig.get_instance()
+
+    dataset = Dataset(config.test_dirs)
+
+    model_configs = []
+
+    for seed in [0, 1, 2]:
+        model_configs.extend((
+            ModelConfig(
+                name="res50_loc",
+                model_type=SeResNext50_Unet_Loc,
+                version="0",
+                seed=seed
+            ),
+            ModelConfig(
+                name="dpn92_loc",
+                model_type=Dpn92_Unet_Loc,
+                version="0",
+                seed=seed
+            ),
+            ModelConfig(
+                name="se154_loc",
+                model_type=SeNet154_Unet_Loc,
+                version="0",
+                seed=seed
+            ),
+            ModelConfig(
+                name="res34_loc",
+                model_type=Res34_Unet_Loc,
+                version="1",
+                seed=seed
+            )
+        ))
+
+    LocalizationPredictor(model_configs,
+                          config.predictions_dir,
+                          dataset)
+
+    elapsed = timeit.default_timer() - t0
+    log(':hourglass: : {:.3f} min'.format(elapsed / 60))
