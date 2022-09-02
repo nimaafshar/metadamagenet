df747cfbab5375a01063fb489ac30514e11b9903
nimaafshar <nima.afsharh@gmail.com>
2022-09-02 12:47:17 +0430

    minor refactor

--- src/util/utils.py
+++ src/util/utils.py
@@ -6,19 +6,38 @@ class AverageMeter(object):
     """Computes and stores the average and current value"""
 
     def __init__(self):
-        self.reset()
+        self._val: float = 0
+        self._avg: float = 0
+        self._sum: float = 0
+        self._count: float = 0
 
     def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+        self._val = 0
+        self._avg = 0
+        self._sum = 0
+        self._count = 0
 
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+    def update(self, value: float, n: int = 1) -> None:
+        self._val = value
+        self._sum += value * n
+        self._count += n
+        self._avg = self._sum / self._count
+
+    @property
+    def val(self) -> float:
+        return self._val
+
+    @property
+    def sum(self) -> float:
+        return self._sum
+
+    @property
+    def count(self) -> float:
+        return self._count
+
+    @property
+    def avg(self) -> float:
+        return self._avg
 
 
 def normalize_colors(x: npt.ArrayLike) -> npt.NDArray[np.float32]:
@@ -28,5 +47,3 @@ def normalize_colors(x: npt.ArrayLike) -> npt.NDArray[np.float32]:
     x /= 127
     x -= 1
     return x
-
-
