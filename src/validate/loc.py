4e0bbd6095a10343fc9344ae7c5d8a49dda602e8
nimaafshar <nima.afsharh@gmail.com>
2022-09-02 11:17:24 +0430

    move tensors to cpu for augmentation

--- src/validate/loc.py
+++ src/validate/loc.py
@@ -25,17 +25,18 @@ class LocalizationValidator(Validator):
         with torch.no_grad():
             for i, (img_batch, msk_batch) in enumerate(iterator):
                 msk_batch: torch.FloatTensor = msk_batch.cuda(non_blocking=True)
-                img_batch: torch.BoolTensor = img_batch.cuda(non_blocking=True)
+                img_batch: torch.BoolTensor = img_batch.cpu()
 
                 msk_pred: torch.FloatTensor
 
                 if self._test_time_augmentor is not None:
-                    augmented_batch: torch.FloatTensor = self._test_time_augmentor.augment(img_batch)
+                    augmented_batch: torch.FloatTensor = self._test_time_augmentor.augment(img_batch).cuda(
+                        non_blocking=True)
                     augmented_out_batch: torch.FloatTensor = self._model(augmented_batch)
                     augmented_msk_pred: torch.FloatTensor = torch.sigmoid(augmented_out_batch[:, 0, ...])
-                    msk_pred = self._test_time_augmentor.aggregate(augmented_msk_pred)
+                    msk_pred = self._test_time_augmentor.aggregate(augmented_msk_pred.cpu()).cuda(non_blocking=True)
                 else:
-                    out_batch: torch.FloatTensor = self._model(img_batch)
+                    out_batch: torch.FloatTensor = self._model(img_batch.cuda(non_blocking=True))
                     msk_pred: torch.FloatTensor = torch.sigmoid(out_batch[:, 0, ...])
 
                 dice_scores = dice_batch(msk_batch[:, 0, ...], msk_pred > self._evaluation_dice_thr)
