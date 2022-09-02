8c09aea1a155ad0176fdd10f85febfa9b3ded11b
nimaafshar <nima.afsharh@gmail.com>
2022-09-02 14:45:50 +0430

    add argument parser

--- examples/validate/loc/resnet34_unet_loc.py
+++ examples/validate/loc/resnet34_unet_loc.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import timeit
 
 import torch
@@ -24,41 +24,34 @@ class Resnet34LocValidator(LocalizationValidator):
 
 
 if __name__ == "__main__":
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("seed", help="model configuration seed", type=int)
+    parser.add_argument("--test-time-augment", help="use test-time augmentation", action="store_true")
+    args = parser.parse_args()
+
     t0 = timeit.default_timer()
-
-    seed = int(sys.argv[1])
-
     GeneralConfig.load()
-
     config = GeneralConfig.get_instance()
-
     image_dataset: ImageDataset = ImageDataset(config.test_dirs)
     image_dataset.discover()
-
     dataset: LocalizationDataset = LocalizationDataset(image_dataset, post_version_prob=1)
-
     dataloader: DataLoader = DataLoader(dataset,
                                         batch_size=4,
                                         num_workers=6,
                                         shuffle=False,
                                         pin_memory=True)
-
     model_config = ModelConfig(
         name='res34_loc',
         empty_model=torch.nn.DataParallel(Res34_Unet_Loc().cuda()).cuda(),
         version='1',
-        seed=seed
+        seed=args.seed
     )
-
     validation_config = ValidationConfig(
         model_config=model_config,
         dataloader=dataloader,
-        test_time_augmentor=FourFlips()
+        test_time_augmentor=FourFlips() if args.test_time_augment else None
     )
-
     validator: LocalizationValidator = Resnet34LocValidator(validation_config)
-
     validator.validate()
-
     elapsed = timeit.default_timer() - t0
     log(':hourglass: : {:.3f} min'.format(elapsed / 60))
