fddf2bc8300aaf729396d33c752556ad7608e1a2
nimaafshar <nima.afsharh@gmail.com>
2022-09-04 17:45:18 +0430

    import to package root

---
+++ metadamagenet/models/unet/__init__.py
@@ -0,0 +1,4 @@
+from .dpn92 import Dpn92UnetDouble, Dpn92UnetLocalization
+from .resnet34 import Resnet34UnetDouble, Resnet34UnetLocalization
+from .senet154 import SENet154UnetDouble, SeNet154UnetLocalization
+from .seresnext50 import SeResNext50UnetDouble, SeResNext50UnetLocalization
