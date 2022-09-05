5fe7d95b0fec8db2325116c8d4c49d58760fc3a5
nimaafshar <nima.afsharh@gmail.com>
2022-09-05 20:14:01 +0430

    add ModelWrapper for Resnet34 models

--- metadamagenet/wrapper/resnet34.py
+++ metadamagenet/wrapper/resnet34.py
@@ -1,3 +1,4 @@
+import abc
 from typing import Tuple
 from torchvision.models import resnet34
 from torch import nn
@@ -11,50 +12,34 @@ from ..unet.resnet34 import Resnet34Unet
 from torchvision.models import ResNet
 
 
-class Resnet34Wrapper(LocalizerModelWrapper):
+class Resnet34Wrapper(LocalizerModelWrapper, abc.ABC):
+    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
+        checkpoint = Checkpoint(
+            model_name=self.model_name,
+            version=version,
+            seed=seed
+        )
+        manager = Manager.get_instance()
+        state_dict, metadata = manager.load_checkpoint(checkpoint)
+        empty_model: nn.Module = nn.DataParallel(self.unet_type(Resnet34Unet(resnet34())))
+        empty_model.load_state_dict(state_dict, strict=True)
+        return empty_model, metadata
+
+    def from_unet(self, unet: Unet) -> Localizer:
+        return nn.DataParallel(self.unet_type(unet)), Metadata()
+
+    def from_backbone(self, backbone: ResNet) -> Localizer:
+        return nn.DataParallel((self.unet_type(Resnet34Unet(backbone))))
+
+
+class Resnet34LocalizerWrapper(Resnet34Wrapper, LocalizerModelWrapper):
     @property
     def model_name(self) -> str:
         return "Resnet34UnetLocalizer"
 
-    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
-        checkpoint = Checkpoint(
-            model_name=self.model_name,
-            version=version,
-            seed=seed
-        )
-        manager = Manager.get_instance()
-        state_dict, metadata = manager.load_checkpoint(checkpoint)
-        empty_model: nn.Module = nn.DataParallel(self.unet_type(Resnet34Unet(resnet34())))
-        empty_model.load_state_dict(state_dict, strict=True)
-        return empty_model, metadata
 
-    def from_unet(self, unet: Unet) -> Localizer:
-        return nn.DataParallel(self.unet_type(unet)), Metadata()
-
-    def from_backbone(self, backbone: ResNet) -> Localizer:
-        return nn.DataParallel((self.unet_type(Resnet34Unet(backbone))))
-
-
-class Resnet34ClassifierWrapper(ModelWrapper, LocalizerModelWrapper):
+class Resnet34ClassifierWrapper(Resnet34Wrapper, ClassifierModelWrapper):
 
     @property
     def model_name(self) -> str:
         return "Resnet34UnetClassifier"
-
-    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
-        checkpoint = Checkpoint(
-            model_name=self.model_name,
-            version=version,
-            seed=seed
-        )
-        manager = Manager.get_instance()
-        state_dict, metadata = manager.load_checkpoint(checkpoint)
-        empty_model: nn.Module = nn.DataParallel(self.unet_type(Resnet34Unet(resnet34())))
-        empty_model.load_state_dict(state_dict, strict=True)
-        return empty_model, metadata
-
-    def from_unet(self, unet: Unet) -> Localizer:
-        return nn.DataParallel(self.unet_type(unet)), Metadata()
-
-    def from_backbone(self, backbone: ResNet) -> Localizer:
-        return nn.DataParallel((self.unet_type(Resnet34Unet(backbone))))
