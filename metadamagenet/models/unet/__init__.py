a5c850707822e2438dd2b77030b9102d19b9a731
nimaafshar <nima.afsharh@gmail.com>
2022-09-06 20:00:55 +0430

    checkpoint + metadata added

--- metadamagenet/models/unet/__init__.py
+++ metadamagenet/models/unet/__init__.py
@@ -1,4 +1,5 @@
-from .dpn92 import Dpn92UnetDouble, Dpn92UnetLocalization
-from .resnet34 import Resnet34UnetDouble, Resnet34UnetLocalization
-from .senet154 import SENet154UnetDouble, SeNet154UnetLocalization
-from .seresnext50 import SeResNext50UnetDouble, SeResNext50UnetLocalization
+from .base import Unet, Localizer, Classifier
+from .resnet34 import Resnet34Unet
+from .seresnext50 import SeResnext50Unet
+from .dpn92 import Dpn92Unet
+from .senet154 import SeNet154Unet
