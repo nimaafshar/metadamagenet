a1e037f0d62a21a2b1c8b38ca81ee1e2ea2794ff
nimaafshar <nima.afsharh@gmail.com>
2022-09-06 19:59:42 +0430

    simplify methods

--- metadamagenet/wrapper/senet154.py
+++ metadamagenet/wrapper/senet154.py
@@ -4,49 +4,31 @@ from typing import Tuple
 import torch
 from torch import nn
 
-
 from .wrapper import ModelWrapper, LocalizerModelWrapper, ClassifierModelWrapper
 from metadamagenet.models.metadata import Metadata
-from metadamagenet.models.checkpoint import Checkpoint
-from metadamagenet.models.manager import Manager
+from metadamagenet.models.unet import Unet
 from metadamagenet.models.unet import SeNet154Unet
 from metadamagenet.models.senet import SENet, senet154
 
 
 class SeNet154Wrapper(ModelWrapper, abc.ABC):
-    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
-        checkpoint = Checkpoint(
-            model_name=self.model_name,
-            version=version,
-            seed=seed
-        )
-        manager = Manager.get_instance()
-        state_dict, metadata = manager.load_checkpoint(checkpoint)
-        empty_model: nn.Module = nn.DataParallel(self.unet_type(SeNet154Unet(senet154(pretrained=False))))
-        empty_model.load_state_dict(state_dict, strict=True)
-        return empty_model, metadata
+    data_parallel = True
 
-    def from_unet(self, unet: SeNet154Unet) -> Tuple[nn.Module, Metadata]:
-        return nn.DataParallel(self.unet_type(unet)), Metadata()
+    def empty_unet(self) -> Unet:
+        return SeNet154Unet(senet154(pretrained=None))
 
-    def from_backbone(self, backbone: SENet) -> Tuple[nn.Module, Metadata]:
-        """
-        :param backbone: a senet154 module
-        """
-        return nn.DataParallel((self.unet_type(SeNet154Unet(backbone)))), Metadata()
+    def unet_with_pretrained_backbone(self, backbone: nn.Module) -> Unet:
+        return SeNet154Unet(senet154())
 
 
 class SeNet154LocalizerWrapper(SeNet154Wrapper, LocalizerModelWrapper):
-    @property
-    def model_name(self) -> str:
-        return "SeNet154UnetLocalizer"
+    model_name = "SeNet154UnetLocalizer"
+    input_size = (480, 480)
 
 
 class SeNet154ClassifierWrapper(SeNet154Wrapper, ClassifierModelWrapper):
-
-    @property
-    def model_name(self) -> str:
-        return "SeNet154UnetClassifier"
+    model_name = "SeNet154UnetClassifier"
+    input_size = 448, 448
 
     def apply_activation(self, x: torch.Tensor) -> torch.Tensor:
-        return torch.softmax(x,dim=1)
+        return torch.softmax(x, dim=1)
--- metadamagenet/wrapper/seresnext50.py
+++ metadamagenet/wrapper/seresnext50.py
@@ -14,62 +14,23 @@ from metadamagenet.models.senet import se_resnext50_32x4d, SENet
 
 
 class SeResnext50Wrapper(ModelWrapper, abc.ABC):
-    pass
+    input_size = 512, 512
+
+    def empty_unet(self) -> Unet:
+        return SeResnext50Unet(se_resnext50_32x4d(pretrained=None))
+
+    def unet_with_pretrained_backbone(self, backbone: nn.Module) -> Unet:
+        return SeResnext50Unet(se_resnext50_32x4d())
 
 
 class SeResnext50LocalizerWrapper(SeResnext50Wrapper, LocalizerModelWrapper):
-    @property
-    def model_name(self) -> str:
-        return "SeResnext50UnetLocalizer"
-
-    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
-        checkpoint = Checkpoint(
-            model_name=self.model_name,
-            version=version,
-            seed=seed
-        )
-        manager = Manager.get_instance()
-        state_dict, metadata = manager.load_checkpoint(checkpoint)
-        empty_model: nn.Module = self.unet_type(SeResnext50Unet(se_resnext50_32x4d(pretrained=None)))
-        empty_model.load_state_dict(state_dict, strict=True)
-        return empty_model, metadata
-
-    def from_unet(self, unet: SeResnext50Unet) -> Tuple[nn.Module, Metadata]:
-        return self.unet_type(unet), Metadata()
-
-    def from_backbone(self, backbone: SENet) -> Tuple[nn.Module, Metadata]:
-        """
-        :param backbone: a se_resnext50_32x4d module
-        """
-        return self.unet_type(SeResnext50Unet(backbone)), Metadata()
+    model_name = "SeResnext50UnetLocalizer"
+    data_parallel = False
 
 
 class SeResnext50ClassifierWrapper(SeResnext50Wrapper, ClassifierModelWrapper):
-
-    @property
-    def model_name(self) -> str:
-        return "SeResnext50UnetClassifier"
-
-    def from_checkpoint(self, version: str, seed: int) -> Tuple[nn.Module, Metadata]:
-        checkpoint = Checkpoint(
-            model_name=self.model_name,
-            version=version,
-            seed=seed
-        )
-        manager = Manager.get_instance()
-        state_dict, metadata = manager.load_checkpoint(checkpoint)
-        empty_model: nn.Module = nn.DataParallel(self.unet_type(SeResnext50Unet(se_resnext50_32x4d(pretrained=None))))
-        empty_model.load_state_dict(state_dict, strict=True)
-        return empty_model, metadata
-
-    def from_unet(self, unet: SeResnext50Unet) -> Tuple[nn.Module, Metadata]:
-        return nn.DataParallel(self.unet_type(unet)), Metadata()
-
-    def from_backbone(self, backbone: SENet) -> Tuple[nn.Module, Metadata]:
-        """
-        :param backbone: a se_resnext50_32x4d module
-        """
-        return nn.DataParallel((self.unet_type(SeResnext50Unet(backbone)))), Metadata()
+    data_parallel = True
+    model_name = "SeResnext50UnetClassifier"
 
     def apply_activation(self, x: torch.Tensor) -> torch.Tensor:
         return torch.softmax(x, dim=1)
