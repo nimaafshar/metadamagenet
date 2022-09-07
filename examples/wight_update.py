dcd7cde84934d8f6009852d5db7393fbe363ca03
nimaafshar <nima.afsharh@gmail.com>
2022-09-07 12:15:10 +0430

    remove redundant code

--- examples/wight_update.py
+++ examples/wight_update.py
@@ -7,105 +7,41 @@ self._grad_scaler.step(self._optimizer)
 self._grad_scaler.update()
 
 # resnet34 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # senet154 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # seresnext50 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # dpn92 unet localization
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
 
 # resnet34 unet localization
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # senet 154 unet localization
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # seresnext 50 unet localization
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
 
 # tune
 # dpn92 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # resnet 34 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # se154 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # seresnext 50 unet double
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.999)
 
 # dpn92 unet loc
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
+
 # seresnext 50 unet loc
-self._optimizer.zero_grad()
-        self._grad_scaler.scale(loss).backward()
-        self._grad_scaler.unscale_(self._optimizer)
-        torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
-        self._grad_scaler.step(self._optimizer)
-        self._grad_scaler.update()
+torch.nn.utils.clip_grad_norm_(self._model.parameters(), 1.1)
