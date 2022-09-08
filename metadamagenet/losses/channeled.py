bdd489fb60e660036640dee7b167cdbdb1593e45
nimaafshar <nima.afsharh@gmail.com>
2022-09-08 13:08:31 +0430

    monitor losses

--- metadamagenet/losses/channeled.py
+++ metadamagenet/losses/channeled.py
@@ -1,21 +1,61 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import torch
 from torch import nn
 
+from .monitored import MonitoredLoss
+from metadamagenet.utils import AverageMeter
 
-class ChanneledLoss(nn.Module):
-    def __init__(self, *channel_losses: Tuple[nn.Module, float]):
+
+class ChanneledLoss(MonitoredLoss):
+    def __init__(self, *channel_losses: Tuple[nn.Module, float], monitored: bool = False):
         super().__init__()
         losses: Tuple[nn.Module]
         weights: Tuple[float]
         losses, weights = zip(*channel_losses)
         self.losses: nn.ModuleList = nn.ModuleList(losses)
         self.register_buffer("weights", torch.FloatTensor(weights))
+        self._monitor: bool = monitored
+        if self._monitor:
+            self._meters: List[AverageMeter] = [AverageMeter() for _ in len(losses)]
+            self._total_loss_meter = AverageMeter()
 
     def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         assert outputs.size(1) == targets.size(1) == len(self.losses), \
             "number of channels with number of losses do not match"
 
         loss_values = [loss(outputs[:, i, ...], targets[:, i, ...]) for i, loss in enumerate(self.losses)]
-        return torch.dot(torch.tensor(loss_values), self.weights)
+        weighted_loss = torch.dot(torch.tensor(loss_values), self.weights)
+
+        if self._monitor:
+            for i, loss_value in enumerate(loss_values):
+                self._meters[i].update(loss_value.item(), outputs.size()[0])
+            self._total_loss_meter.update(weighted_loss.item(), outputs.size()[0])
+
+        return weighted_loss
+
+    @property
+    def monitored(self) -> bool:
+        return self._monitor
+
+    def last_values(self) -> str:
+        if not self._monitor:
+            return "not monitored"
+        children_last_values = []
+        for i, name in len(self._meters):
+            if isinstance(self.losses[i], MonitoredLoss) and self.losses[i].monitored:
+                children_last_values.append(self.losses[i].last_values)
+            else:
+                children_last_values.append(f"{self._meters[i].val:.4f} ({self._meters[i].avg:.4f})")
+        return f"Total: {self._total_loss_meter.val:.4f} [{'; '.join(children_last_values)}]"
+
+    def aggregate(self) -> str:
+        if not self._monitor:
+            return "not monitored"
+        children_aggregated = []
+        for i, name in len(self._meters):
+            if isinstance(self.losses[i], MonitoredLoss) and self.losses[i].monitored:
+                children_aggregated.append(self.losses[i].aggregate())
+            else:
+                children_aggregated.append(f"{self._meters[i].avg:.4f}")
+        return f"Total: {self._total_loss_meter.avg:.4f} [{'; '.join(children_aggregated)}]"
