db56fe97ff8fc38e6941f5745980ac29e8d093da
nimaafshar <nima.afsharh@gmail.com>
2022-09-09 14:26:43 +0430

    add metrics

---
+++ metadamagenet/metrics/base.py
@@ -0,0 +1,88 @@
+import abc
+
+import torch
+
+
+class FloatMetric:
+    """
+    Compute a float metric in multiple steps
+    """
+
+    def __init__(self):
+        self._val: float = 0
+        self._avg: float = 0
+        self._sum: float = 0
+        self._count: float = 0
+
+    def reset(self) -> None:
+        """
+        reset metric for another round of calculation
+        """
+        self._val = 0
+        self._avg = 0
+        self._sum = 0
+        self._count = 0
+
+    @abc.abstractmethod
+    def update(self, outputs: torch.Tensor, targets: torch.Tensor) -> None:
+        """
+        update metric based on a batch of outputs and a batch of targets
+        :param outputs: batch of outputs
+        :param targets: batch of targets
+        :return: None
+        """
+        pass
+
+    def _update(self, value: float, n: int = 1) -> None:
+        """
+        submit some value n times
+        :param value:
+        :param n:
+        :return:
+        """
+        self._val = value
+        self._sum += value * n
+        self._count += n
+        self._avg = self._sum / self._count
+
+    @property
+    def last(self) -> float:
+        """
+        :return: last value
+        """
+        return self._val
+
+    @property
+    def sum(self) -> float:
+        """
+        :return: sum of values
+        """
+        return self._sum
+
+    @property
+    def count(self) -> float:
+        """
+        :return: count of values
+        """
+        return self._count
+
+    @property
+    def avg(self) -> float:
+        """
+        :return: average of values
+        """
+        return self._avg
+
+    @property
+    def status(self) -> str:
+        """
+        :return: last status of metric formatted as string
+        """
+        return f"{self._val:.4f} ({self._avg:.4f})"
+
+    @property
+    def avg_status(self) -> str:
+        """
+        :return: average status of metric formatted as string
+        """
+        return f"{self._avg:.4f}"
--- metadamagenet/metrics/metrics.py
+++ metadamagenet/metrics/metrics.py
@@ -1,62 +1,70 @@
-import abc
-from typing import List
+import torch
 
-import numpy as np
-import numpy.typing as npt
-
-from src.losses import dice
+from .base import FloatMetric
+from metadamagenet.losses.dice import dice_round, dice_batch
+from metadamagenet.losses.epsilon import eps
 
 
-class MetricCalculator(abc.ABC):
-    @abc.abstractmethod
-    def update(self, msk: npt.NDArray, msk_pred: npt.NDArray) -> None:
+class DiceRound(FloatMetric):
+    def __init__(self, channel: int = 0):
+        super().__init__()
+        self._channel: int = channel
+
+    def update(self, output_batch: torch.Tensor, targets_batch: torch.Tensor) -> None:
         """
-        :param msk: mask predicted by model
-        :param msk_pred: correct mask
-        :return: None
+        :param output_batch: (B,C,H,W) C >= 1
+        :param targets_batch: same dimentions as output_batch
         """
-        pass
-
-    @abc.abstractmethod
-    def aggregate(self) -> float:
-        pass
-
-    @abc.abstractmethod
-    def reset(self) -> None:
-        pass
+        # msk_0_probs = torch.sigmoid(output_batch[:, 0, ...])
+        dice_sc = 1 - dice_round(output_batch[:, self._channel, ...],
+                                 targets_batch[:, self._channel, ...], per_image=True)
+        self._update(float(dice_sc.mean()), output_batch.size(0))
 
 
-class DiceCalculator(MetricCalculator):
+class Dice(FloatMetric):
+    def __init__(self, threshold: float, channel: int = 0):
+        super().__init__()
+        self._threshold: float = threshold
+        self._channel: int = channel
 
-    def __init__(self, threshold=0.3):
-        self._dices: List[float] = []
-        self._threshold = threshold
-
-    def update(self, msk: npt.NDArray, msk_pred: npt.NDArray) -> None:
-        self._dices.append(dice(msk, msk_pred > self._threshold))
-
-    def aggregate(self) -> float:
-        return float(np.mean(self._dices))
-
-    def reset(self) -> None:
-        self._dices.clear()
+    def update(self, output_batch: torch.Tensor, targets_batch: torch.Tensor) -> None:
+        """
+        :param output_batch: (B,C,H,W) C >= channel
+        :param targets_batch: same dimensions as output_batch
+        """
+        # msk_0_probs = torch.sigmoid(output_batch[:, 0, ...])
+        dice_scores = dice_batch(output_batch[:, self._channel, ...],
+                                 targets_batch[:, self._channel, ...] > self._threshold)
+        self._update(float(dice_scores.mean()), output_batch.size(0))
 
 
-class F1ScoreCalculator(MetricCalculator):
+class F1Score(FloatMetric):
     def __init__(self):
-        self._tp: float = 0.0
-        self._fn: float = 0.0
-        self._fp: float = 0.0
+        super().__init__()
+        self._f1_scores = torch.zeros((4,))
 
-    def update(self, msk: npt.NDArray, msk_pred: npt.NDArray) -> None:
-        self._tp += np.logical_and(msk > 0, msk_pred > 0).sum()
-        self._fn += np.logical_and(msk < 1, msk_pred > 0).sum()
-        self._fp += np.logical_and(msk > 0, msk_pred < 1).sum()
+    def update(self, outputs: torch.Tensor, targets: torch.Tensor) -> None:
+        label_outputs = outputs[:, 1:, ...].argmax(dim=1)
+        label_targets = targets[:, 1:, ...].argmax(dim=1)
+        # filter targets with masks
+        targ = label_targets * (targets[:, 0, ...] > 0)
+        # TODO: filter with preloaded loc mask
+        # filter predictions with masks
+        out = label_outputs * (targets[:, 0, ...] > 0)
+        target_one_hot = torch.nn.functional.one_hot(targ.flatten())
+        out_one_hot = torch.nn.functional.one_hot(out.flatten())
+        tp = torch.logical_and(out_one_hot == 1, target_one_hot == 1).sum(dim=0)
+        fn = torch.logical_and(out_one_hot != 1, target_one_hot == 1).sum(dim=0)
+        fp = torch.logical_and(out_one_hot == 1, target_one_hot != 1).sum(dim=0)
+        f1_scores = 2 * tp / (2 * tp + fp + fn)
+        self._f1_scores += f1_scores * outputs.size(0)
+        f1_final = 4 / torch.sum(1.0 / (f1_scores + eps))
+        self._update(float(f1_final), outputs.size(0))
 
-    def aggregate(self) -> float:
-        return 2 * self._tp / (2 * self._tp + self._fp + self._fn)
-
-    def reset(self):
-        self._tp: float = 0.0
-        self._fn: float = 0.0
-        self._fp: float = 0.0
+    def avg_status(self) -> str:
+        avg_f1_scores = self._f1_scores / self.count
+        return f'Total: {self.avg},' \
+               f'[{avg_f1_scores[0]:.4f},' \
+               f'{avg_f1_scores[1]:.4f},' \
+               f'{avg_f1_scores[2]:.4f},' \
+               f'{avg_f1_scores[3]:.4f}]'
