a9d516bec14a2ca842796209ef2858c2caced081
nimaafshar <nima.afsharh@gmail.com>
2022-09-11 11:26:00 +0430

    inverse layer0 metrics

--- metadamagenet/metrics/dice.py
+++ metadamagenet/metrics/dice.py
@@ -5,33 +5,35 @@ from ..losses.dice import dice_round, dice_batch
 
 
 class DiceRound(FloatMetric):
-    def __init__(self, channel: int = 0):
+    def __init__(self, channel: int = 0, inverse: bool = False):
         super().__init__()
         self._channel: int = channel
+        self._inverse: bool = inverse
 
     def update(self, output_batch: torch.Tensor, targets_batch: torch.Tensor) -> None:
         """
         :param output_batch: (B,C,H,W) C >= 1
-        :param targets_batch: same dimentions as output_batch
+        :param targets_batch: same dimensions as output_batch
         """
-        # msk_0_probs = torch.sigmoid(output_batch[:, 0, ...])
-        dice_sc = 1 - dice_round(output_batch[:, self._channel, ...],
-                                 targets_batch[:, self._channel, ...], per_image=True)
+        out = 1 - output_batch[:, self._channel, ...] if self._inverse else output_batch[:, self._channel, ...]
+        tar = 1 - targets_batch[:, self._channel, ...] if self._inverse else targets_batch[:, self._channel, ...]
+        dice_sc = 1 - dice_round(out, tar, per_image=True)
         self._update(float(dice_sc.mean()), output_batch.size(0))
 
 
 class Dice(FloatMetric):
-    def __init__(self, threshold: float, channel: int = 0):
+    def __init__(self, threshold: float, channel: int = 0, inverse: bool = False):
         super().__init__()
         self._threshold: float = threshold
         self._channel: int = channel
+        self._inverse: bool = inverse
 
     def update(self, output_batch: torch.Tensor, targets_batch: torch.Tensor) -> None:
         """
         :param output_batch: (B,C,H,W) C >= channel
         :param targets_batch: same dimensions as output_batch
         """
-        # msk_0_probs = torch.sigmoid(output_batch[:, 0, ...])
-        dice_scores = dice_batch(output_batch[:, self._channel, ...],
-                                 targets_batch[:, self._channel, ...] > self._threshold)
+        out = 1 - output_batch[:, self._channel, ...] if self._inverse else output_batch[:, self._channel, ...]
+        tar = 1 - targets_batch[:, self._channel, ...] if self._inverse else targets_batch[:, self._channel, ...]
+        dice_scores = dice_batch(out, tar > self._threshold)
         self._update(float(dice_scores.mean()), output_batch.size(0))
