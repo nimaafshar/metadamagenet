0ef24c11b077f32424d31c5912583c0999833e7d
nimaafshar <nima.afsharh@gmail.com>
2022-09-11 11:12:32 +0430

    fix Classification Dataset

--- metadamagenet/dataset/classification.py
+++ metadamagenet/dataset/classification.py
@@ -1,6 +1,4 @@
-import abc
-import random
-from typing import Tuple, Union, Optional, Dict
+from typing import Optional, Dict
 
 import numpy as np
 import numpy.typing as npt
@@ -17,11 +15,15 @@ from .image_dataset import ImageDataset
 from .base import Dataset
 
 
+def get_one_hot(targets: npt.NDArray, nb_classes: int):
+    res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+    return res.reshape(list(targets.shape) + [nb_classes])
+
+
 class ClassificationDataset(Dataset):
     def __init__(self,
                  image_dataset: ImageDataset,
                  do_dilation: bool = False,
-                 inverse_msk0: bool = False,
                  augmentations: Optional[Pipeline] = None):
         """
         Train Dataset
@@ -30,7 +32,6 @@ class ClassificationDataset(Dataset):
         :param augmentations: pipeline of augmentations
         """
         self._do_dilation: bool = do_dilation
-        self._inverse_msk0: bool = inverse_msk0
         super().__init__(image_dataset, augmentations)
 
     def __getitem__(self, identifier: int) -> Dict[str, npt.NDArray]:
@@ -44,27 +45,27 @@ class ClassificationDataset(Dataset):
         """
         image_data: ImageData = self._image_dataset[identifier]
 
-        img: npt.NDArray
-        msk: npt.NDArray
-
         # read pre_disaster image and msk
         img_pre: npt.NDArray = cv2.imread(str(image_data.image(DataTime.PRE)), cv2.IMREAD_COLOR)
-        msk0: npt.NDArray = cv2.imread(str(image_data.mask(DataTime.PRE)), cv2.IMREAD_UNCHANGED)
+        # msk_pre: npt.NDArray = cv2.imread(str(image_data.mask(DataTime.PRE)), cv2.IMREAD_UNCHANGED)
 
         # read post_disaster image and mask
-        # TODO: inspect this 255
         img_post: npt.NDArray = cv2.imread(str(image_data.image(DataTime.POST)), cv2.IMREAD_COLOR)
-        label_msk: npt.NDArray = (cv2.imread(str(image_data.mask(DataTime.POST)), cv2.IMREAD_UNCHANGED) > 0) * 255
+        msk_post: npt.NDArray = cv2.imread(str(image_data.mask(DataTime.POST)), cv2.IMREAD_UNCHANGED)  # * 255
+        # values 0-4
 
         # convert label_mask into four black and white masks
-        msk1: npt.NDArray = np.zeros_like(label_msk)
-        msk2: npt.NDArray = np.zeros_like(label_msk)
-        msk3: npt.NDArray = np.zeros_like(label_msk)
-        msk4: npt.NDArray = np.zeros_like(label_msk)
-        msk1[label_msk == 1] = 255
-        msk2[label_msk == 2] = 255
-        msk3[label_msk == 3] = 255
-        msk4[label_msk == 4] = 255
+        msk0: npt.NDArray = np.zeros_like(msk_post)
+        msk1: npt.NDArray = np.zeros_like(msk_post)
+        msk2: npt.NDArray = np.zeros_like(msk_post)
+        msk3: npt.NDArray = np.zeros_like(msk_post)
+        msk4: npt.NDArray = np.zeros_like(msk_post)
+
+        msk0[msk_post == 0] = 1
+        msk1[msk_post == 1] = 1
+        msk2[msk_post == 2] = 1
+        msk3[msk_post == 3] = 1
+        msk4[msk_post == 4] = 1
 
         if self._augments is not None:
             img_batch = {
@@ -92,10 +93,9 @@ class ClassificationDataset(Dataset):
         msk4 = msk4[..., np.newaxis]
 
         msk = np.concatenate([msk0, msk1, msk2, msk3, msk4], axis=2)
-        msk = (msk > 127)
 
         if self._do_dilation:
-            msk[..., 0] = self._inverse_msk0
+            msk[..., 0] = True
             msk[..., 1] = dilation(msk[..., 1], square(5))
             msk[..., 2] = dilation(msk[..., 2], square(5))
             msk[..., 3] = dilation(msk[..., 3], square(5))
@@ -104,40 +104,14 @@ class ClassificationDataset(Dataset):
             msk[..., 3][msk[..., 2]] = False
             msk[..., 4][msk[..., 2]] = False
             msk[..., 4][msk[..., 3]] = False
-            msk[..., 0][msk[..., 1:].max(axis=2)] = not self._inverse_msk0
+            msk[..., 0][msk[..., 1:].max(axis=2)] = False
 
         msk = msk * 1
 
-        label_msk = msk.argmax(axis=2)
         img = np.concatenate([img_pre, img_post], axis=2)
-
         img = normalize_colors(img)
 
         img = torch.from_numpy(img.transpose((2, 0, 1))).float()
         msk = torch.from_numpy(msk.transpose((2, 0, 1))).long()
 
-        return {
-            'img': img,
-            'msk': msk,
-            'label_msk': label_msk
-        }
-
-
-class ClassificationValidationDataset(ClassificationDataset):
-    """
-    Validation Dataset for classification
-    """
-
-    def __init__(self, image_dataset: ImageDataset,
-                 augmentations: Optional[Pipeline] = None):
-        """
-        :param image_dataset: dataset of images
-        :param augmentations: pipeline of augmentations
-        """
-        super().__init__(image_dataset, False, False, augmentations)
-
-    def __getitem__(self, identifier: int):
-        image_data: ImageData = self._image_dataset[identifier]
-        data: dict = super(ClassificationValidationDataset, self).__getitem__(identifier)
-        data['msk_loc'] = cv2.imread(str(image_data.localization_mask), cv2.IMREAD_UNCHANGED) > (0.3 * 255)
-        return data
+        return img, msk
