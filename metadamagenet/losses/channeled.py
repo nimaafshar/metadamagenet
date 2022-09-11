1ed17d7932267b9ffcf3a139e312392221ef6a7b
nimaafshar <nima.afsharh@gmail.com>
2022-09-11 19:44:48 +0430

    fix typos

--- metadamagenet/losses/channeled.py
+++ metadamagenet/losses/channeled.py
@@ -28,7 +28,7 @@ class ChanneledLoss(MonitoredLoss):
         weighted_loss = 0
         for i, loss in enumerate(self.losses):
             value = loss(outputs[:, i, ...], targets[:, i, ...])
-            weighted_loss = value * self._weights[i]
+            weighted_loss = value * self.weights[i]
             loss_items.append(value.item())
 
         if self._monitor:
