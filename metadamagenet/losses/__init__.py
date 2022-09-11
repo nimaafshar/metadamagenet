0d36d440218f8c2158d886b4fdfa39ccf80380b3
nimaafshar <nima.afsharh@gmail.com>
2022-09-11 12:05:26 +0430

    fix absolute import bug

--- metadamagenet/losses/__init__.py
+++ metadamagenet/losses/__init__.py
@@ -1,8 +1,8 @@
 from .dice import DiceLoss
 from .bce import StableBCELoss
-from focal import FocalLoss2d
-from jaccard import JaccardLoss
-from lovasz import LovaszLoss, LovaszLossSigmoid
+from .focal import FocalLoss2d
+from .jaccard import JaccardLoss
+from .lovasz import LovaszLoss, LovaszLossSigmoid
 from .utils import WithSigmoid
 from .combo import ComboLoss
 from .channeled import ChanneledLoss
