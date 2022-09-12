a13d262046f3b5d04b5ab36a6fdb499450ec57f8
nimaafshar <nima.afsharh@gmail.com>
2022-09-12 23:21:48 +0430

    define new metric interface

---
+++ metadamagenet/metrics/average.py
@@ -0,0 +1,32 @@
+from .base import Metric
+
+
+class AverageMetric(Metric):
+    def __init__(self):
+        self._avg: float = 0
+        self._sum: float = 0
+        self._count: float = 0
+
+    def update(self, value: float, count: int = 1) -> None:
+        """
+        consider value n times
+        """
+        self._sum += value * count
+        self._count += count
+        self._avg = self._sum / self._count
+
+    def reset(self) -> None:
+        """
+        reset metric for another round of calculation
+        """
+        self._avg = 0
+        self._sum = 0
+        self._count = 0
+
+    @property
+    def till_here(self) -> float:
+        return self._avg
+
+    @property
+    def status_till_here(self) -> str:
+        return f"{self._avg:.4f}"
--- metadamagenet/metrics/base.py
+++ metadamagenet/metrics/base.py
@@ -3,91 +3,41 @@ import abc
 import torch
 
 
-class FloatMetric:
+class Metric:
     """
-    Compute a float metric in multiple steps
+    Interface of a metric
     """
 
-    def __init__(self):
-        self._val: float = 0
-        self._avg: float = 0
-        self._sum: float = 0
-        self._count: float = 0
-
-    def reset(self) -> None:
-        """
-        reset metric for another round of calculation
-        """
-        self._val = 0
-        self._avg = 0
-        self._sum = 0
-        self._count = 0
-
+    @property
     @abc.abstractmethod
-    def update(self, outputs: torch.Tensor, targets: torch.Tensor) -> None:
+    def till_here(self) -> float:
         """
-        update metric based on a batch of outputs and a batch of targets
-        :param outputs: batch of outputs
-        :param targets: batch of targets
-        :return: None
+        metric value till here
         """
         pass
 
-    def _update(self, value: float, n: int = 1) -> None:
-        """
-        submit some value n times
-        :param value:
-        :param n:
-        :return:
-        """
-        self._val = value
-        self._sum += value * n
-        self._count += n
-        self._avg = self._sum / self._count
-
     @property
-    def last(self) -> float:
+    @abc.abstractmethod
+    def status_till_here(self) -> str:
         """
-        :return: last value
+        metric value till here formatted as string
         """
-        return self._val
+        pass
 
-    @property
-    def sum(self) -> float:
+    @abc.abstractmethod
+    def reset(self) -> None:
         """
-        :return: sum of values
+        reset recorded state
         """
-        return self._sum
-
-    @property
-    def count(self) -> float:
-        """
-        :return: count of values
-        """
-        return self._count
-
-    @property
-    def avg(self) -> float:
-        """
-        :return: average of values
-        """
-        return self._avg
-
-    @property
-    def status(self) -> str:
-        """
-        :return: last status of metric formatted as string
-        """
-        return f"{self._val:.4f} ({self._avg:.4f})"
-
-    @property
-    def avg_status(self) -> str:
-        """
-        :return: average status of metric formatted as string
-        """
-        return f"{self._avg:.4f}"
+        pass
 
 
-class AverageMeter(FloatMetric):
-    def update(self, value: int, n: int = 1) -> None:
-        self._update(value, n)
+class ImageMetric(Metric, abc.ABC):
+    @abc.abstractmethod
+    def update_batch(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """
+        calculate and update metric based on a batch of outputs and a batch of targets
+        :param outputs: tensor of shape (B,C,H,W)
+        :param targets: tensor of shape (B,C,H,W)
+        :return: metric value for this batch
+        """
