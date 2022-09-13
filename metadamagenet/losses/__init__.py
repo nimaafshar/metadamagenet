202cb294386b59e779887c6884cf3d6f2cb7aaeb
nimaafshar <nima.afsharh@gmail.com>
2022-09-13 15:03:02 +0430

    integrate losses with new MonitoredLoss interface

--- metadamagenet/losses/__init__.py
+++ metadamagenet/losses/__init__.py
@@ -1,9 +1,11 @@
+from .base import Monitored, MonitoredImageLoss
+from .weighted import WeightedLoss
+from .channeled import ChanneledLoss
+from .utils import WithSigmoid
+
 from .dice import DiceLoss
 from .bce import StableBCELoss
 from .focal import FocalLoss2d
 from .jaccard import JaccardLoss
 from .lovasz import LovaszLoss, LovaszLossSigmoid
-from .utils import WithSigmoid
-from .combo import ComboLoss
-from .channeled import ChanneledLoss
-from .base import Monitored, MonitoredImageLoss
+
--- metadamagenet/losses/weighted.py
+++ metadamagenet/losses/weighted.py
@@ -4,27 +4,38 @@ import torch
 from torch import nn
 
 from .base import MonitoredImageLoss, Monitored
-from ..metrics import AverageMetric
+from ..metrics.average import AverageMetric
 
 
-class ChanneledLoss(MonitoredImageLoss):
-    def __init__(self, *channel_losses: Tuple[nn.Module, float]):
+class WeightedLoss(MonitoredImageLoss):
+    """
+    example:
+    ComboLoss(
+        ("BCE",StableBCELoss(), 1),
+        ("DICE",WithSigmoid(DiceLoss(per_image=False)), 1),
+        ("JACCARD",WithSigmoid(JaccardLoss(per_image=False)), 1),
+        ("Lovasz",LovaszLoss(per_image), 1),
+        ("Lovasz_sig",WithSigmoid(LovaszLossSigmoid(per_image)), 1),
+        ("focal",WithSigmoid(FocalLoss2d()), 1)
+    )
+    """
+
+    def __init__(self, *weighted_losses: Tuple[str, nn.Module, float], monitor: bool = False):
         super().__init__()
         losses: Tuple[nn.Module]
         weights: Tuple[float]
-        losses, weights = zip(*channel_losses)
+        names, losses, weights = zip(*weighted_losses)
         self.losses: nn.ModuleList[MonitoredImageLoss] = nn.ModuleList(
             [loss if isinstance(loss, MonitoredImageLoss) else Monitored(loss) for loss in losses]
         )
         self.register_buffer("weights", torch.FloatTensor(weights))
+        self._names = names
         self._total_loss_meter: AverageMetric = AverageMetric()
 
     def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        assert outputs.size(1) == targets.size(1) == len(self.losses), \
-            f"number of channels ({outputs.size(1)}) and number of losses ({len(self.losses)}) do not match"
         weighted_loss: torch.Tensor = 0
         for i, loss in enumerate(self.losses):
-            weighted_loss += loss(outputs[:, i, ...], targets[:, i, ...]) * self.weights[i]
+            weighted_loss += loss(outputs, targets) * self.weights[i]
         self._total_loss_meter.update(weighted_loss.item(), outputs.size(0))
         return weighted_loss
 
