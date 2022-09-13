3de80844aee242ed6ad828e08598049d081479fc
nimaafshar <nima.afsharh@gmail.com>
2022-09-13 13:37:10 +0430

    add weighted metric

---
+++ metadamagenet/metrics/weighted.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+import torch
+
+from .base import ImageMetric
+from .average import AverageMetric
+
+
+class WeightedImageMetric(ImageMetric):
+    def __init__(self, *metrics: Tuple[str, ImageMetric, float]):
+        super().__init__()
+        self._metrics: Tuple[ImageMetric]
+        self._weights: Tuple[float]
+        self._names: Tuple[str]
+        self._names, self._metrics, self._weights = zip(*metrics)
+        self._average: AverageMetric = AverageMetric()
+
+    def update_batch(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        metric: ImageMetric
+        weighted_sum: torch.Tensor = 0
+        for i, metric in enumerate(self._metrics):
+            weighted_sum += metric.update_batch(outputs, targets) * self._weights[i]
+        self._average.update(weighted_sum.item(), count=outputs.size(0))
+        return weighted_sum
+
+    @property
+    def till_here(self) -> float:
+        return self._average.till_here
+
+    @property
+    def status_till_here(self) -> str:
+        return f"Weighted: {self._average.status_till_here}[" + \
+               ",".join((f"{self._names[i]}: {metric.status_till_here}" for i, metric in enumerate(self._metrics))) + \
+               "]"
+
+    def reset(self) -> None:
+        self._average.reset()
+        for metric in self._metrics:
+            metric.reset()
