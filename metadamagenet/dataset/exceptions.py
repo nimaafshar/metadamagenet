de63f5351d23560f22bf03ac7752701a0689606d
nimaafshar <nima.afsharh@gmail.com>
2022-09-15 20:42:21 +0430

    remove ImageDataset

--- metadamagenet/dataset/exceptions.py
+++
@@ -1,2 +0,0 @@
-class DatasetNotDiscovered(Exception):
-    """when a dataset is not discovered this error is raised"""
--- metadamagenet/dataset/image_data.py
+++ metadamagenet/dataset/image_data.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 import pathlib
 import json
-from typing import List, Tuple
+from typing import List, Tuple, Iterable
 
 from shapely.geometry import Polygon
 from shapely import wkt
 
 from ..configs import DamageType, damage_to_damage_type, GeneralConfig
 from .data_time import DataTime
+from ..logging import log
 
 
 @dataclass
@@ -48,13 +49,6 @@ class ImageData:
         """
         return self.base / GeneralConfig.get_instance().masks_dirname / f'{self.name(time)}_target.png'
 
-    @property
-    def localization_mask(self) -> pathlib.Path:
-        """
-        :return: predicted localization msk for this image
-        """
-        return self.base / GeneralConfig.get_instance().localization_dirname / f'{self.name(DataTime.PRE)}_part1.png'
-
     def polygons(self, time: DataTime = DataTime.PRE) -> List[Tuple[Polygon, DamageType]]:
         """
         list of image polygons and their subtypes
@@ -72,3 +66,44 @@ class ImageData:
             polygons.append((polygon, subtype))
 
         return polygons
+
+
+def discover_directories(directories: Iterable[pathlib.Path], check: bool = True) -> List[ImageData]:
+    results: List[ImageData] = []
+    directory: pathlib.Path
+    for directory in directories:
+        results.extend(discover_directory(directory))
+
+
+def discover_directory(base_directory: pathlib.Path, check: bool = True) -> List[ImageData]:
+    """
+    :param base_directory: directory to discover
+    :param check: check if other files of image data exist
+    :return: list of image datas
+    """
+    results: List[ImageData] = []
+    if not base_directory.is_dir():
+        raise ValueError(f"{base_directory.absolute()} is not a directory")
+
+    log(f":mag: discovering {base_directory.absolute()}...")
+    for file_path in (base_directory / 'images').glob('*_pre_disaster.png'):
+        disaster, identifier, time, _ = file_path.name.split('_')
+        image_data: ImageData = ImageData(base_directory, identifier, disaster)
+        if check:
+            path: pathlib.Path
+            for path in [
+                image_data.image(DataTime.PRE),
+                image_data.image(DataTime.POST),
+                image_data.label(DataTime.PRE),
+                image_data.label(DataTime.POST),
+                image_data.mask(DataTime.PRE),
+                image_data.mask(DataTime.POST)
+            ]:
+                assert path.exists(), f"{path} does not exist"
+                assert path.is_file(), f"{path} is not a file"
+        results.append(image_data)
+
+    if len(results) == 0:
+        log(f"warning: directory {base_directory} is empty")
+
+    return results
