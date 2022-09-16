2d242b4186072e0abe3f597d86e5df0acb706e3f
nimaafshar <nima.afsharh@gmail.com>
2022-09-16 16:28:38 +0430

    CrossEntropyLoss wrapper added

--- metadamagenet/losses/__init__.py
+++ metadamagenet/losses/__init__.py
@@ -2,10 +2,10 @@ from .base import Monitored, MonitoredImageLoss
 from .weighted import WeightedLoss
 from .channeled import ChanneledLoss
 from .utils import WithSigmoid
+from .cross_entropy import SegCCE
 
 from .dice import DiceLoss
 from .bce import StableBCELoss
 from .focal import FocalLoss2d
 from .jaccard import JaccardLoss
 from .lovasz import LovaszLoss, LovaszLossSigmoid
-
