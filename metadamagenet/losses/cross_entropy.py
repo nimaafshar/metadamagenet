2f77286991cd019e8ecf29d5bd43f7b80181b120
nimaafshar <nima.afsharh@gmail.com>
2022-09-16 16:28:38 +0430

    CrossEntropyLoss wrapper added

---
+++ metadamagenet/losses/cross_entropy.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+import torch
+from torch import nn
+
+
+class SegCCE(nn.Module):
+    def __init__(self, weights: Optional[torch.Tensor] = None):
+        super().__init__()
+        self.ce: nn.CrossEntropyLoss = nn.CrossEntropyLoss(weights)
+
+    def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        return self.ce(outputs, targets.argmax(dim=1))
