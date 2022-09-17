a97853663fc45c08828ef9dd630567cedb553405
nimaafshar <nima.afsharh@gmail.com>
2022-09-17 15:14:51 +0430

    f1Score metric changed

--- metadamagenet/metrics/f1.py
+++ metadamagenet/metrics/f1.py
@@ -1,42 +1,31 @@
 import torch
+from torchmetrics import F1Score as TorchMetricsF1Score
 
 from .base import ImageMetric
-from ..losses.epsilon import eps
+from ..metrics import AverageMetric
 
 
 class F1Score(ImageMetric):
-    def __init__(self, start_idx: int, end_idx: int, num_classes: int):
-        """
-        :param start_idx: index of start channel (inclusive)
-        :param end_idx: index of end channel (exclusive)
-        """
+    def __init__(self, num_classes: int):
         super().__init__()
-        self._start_idx: int = start_idx
-        self._end_idx: int = end_idx
-        self._channels: int = self._end_idx - self._start_idx
-        self._f1_scores_sum: torch.Tensor = torch.zeros((self._channels,))
-        self._f1_scores_average: torch.Tensor = torch.zeros((self._channels,))
-        self._count: int = 0
+        self.f1_metric = TorchMetricsF1Score(num_classes=num_classes, mdmc_average='samplewise')
         self._num_classes: int = num_classes
+        self._average_metric: AverageMetric = AverageMetric
 
     def update_batch(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        out = torch.nn.functional.one_hot(outputs.argmax(dim=1), num_classes=5).permute(0, 3, 1, 2)
-        tp = torch.logical_and(targets == 1, out == 1).flatten(start_dim=2).sum(dim=2)
-        fn = torch.logical_and(targets == 1, out != 1).flatten(start_dim=2).sum(dim=2)
-        fp = torch.logical_and(targets != 1, out == 1).flatten(start_dim=2).sum(dim=2)
-        f1_scores = (2 * tp / (2 * tp + fp + fn)).nan_to_num(nan=0).sum(dim=0)[self._start_idx:self._end_idx]
-        self._f1_scores_sum += f1_scores
-        self._count += outputs.size(0)
-        self._f1_scores_average = self._f1_scores_sum / self._count
-        return self._channels / torch.sum(1.0 / ((f1_scores / outputs.size(0)) + eps))
+        output_labels: torch.LongTensor = outputs.argmax(dim=1)
+        target_labels: torch.LongTensor = targets.argmax(dim=1)
+        val: torch.Tensor = self.f1_metric(output_labels.flatten(start_dim=1),
+                                           target_labels.flatten(start_dim=1)).item()
+        self._average_metric.update(val.item(), count=outputs.size(0))
+        return val
 
     def till_here(self) -> float:
-        return (self._channels / torch.sum(1.0 / (self._f1_scores_average + eps))).item()
+        return self._average_metric.till_here()
 
     def status_till_here(self) -> str:
-        return f"total: {self.till_here():.5f}, {self._f1_scores_average}"
+        return self._average_metric.status_till_here()
 
     def reset(self) -> None:
-        self._count = 0
-        self._f1_scores_average = 0.0
-        self._f1_scores_sum = 0.0
+        self.f1_metric.reset()
+        self._average_metric.reset()
