f7cd19d16f789758e8f4c6d4cbe874c26b5e83d3
nimaafshar <nima.afsharh@gmail.com>
2022-09-17 22:01:52 +0430

    add typing

--- metadamagenet/metrics/xview2.py
+++ metadamagenet/metrics/xview2.py
@@ -1,9 +1,10 @@
+from .base import ImageMetric
 from .f1 import LocalizationF1Score, DamageF1Score
 from .weighted import WeightedImageMetric
 
-classification_score = WeightedImageMetric(
+classification_score: ImageMetric = WeightedImageMetric(
     ("F1Loc", LocalizationF1Score(), 0.3),
     ("F1Damage", DamageF1Score(clip_localization_mask=True), 0.7)
 )
 
-localization_score = LocalizationF1Score()
+localization_score: ImageMetric = LocalizationF1Score()
