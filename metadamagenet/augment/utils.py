9d27b6e4c6d58529b9acfce660e413ecdc1553e2
nimaafshar <nima.afsharh@gmail.com>
2022-09-20 20:27:58 +0430

    bugfix: disable requires_grad

--- metadamagenet/augment/utils.py
+++ metadamagenet/augment/utils.py
@@ -4,4 +4,4 @@ import torch
 
 
 def random_float_tensor(size: torch.Size, range_: Tuple[float, float], device: torch.device) -> torch.FloatTensor:
-    return torch.rand(size, device=device) * (range_[1] - range_[0]) + range_[0]
+    return torch.rand(size, device=device, requires_grad=False) * (range_[1] - range_[0]) + range_[0]
