322e53150b59902102656b1ea0dafe8876780924
nimaafshar <nima.afsharh@gmail.com>
2022-09-20 17:00:56 +0430

    bugfix: RGBShift and HSVShift

--- metadamagenet/augment/__init__.py
+++ metadamagenet/augment/__init__.py
@@ -1,7 +1,7 @@
 from .test_time import TestTimeAugmentor, FourFlips
 
 from .base import ImageCollection, Random, Transform, CollectionTransform, OnlyOn, OneOf
-from .enhance import Clahe, Brightness, Contrast, Saturation, RGBShift, RGBShiftState, HSVShift, HSVShiftState
+from .enhance import Clahe, Brightness, Contrast, Saturation, RGBShift, HSVShift
 from .filter import Blur
 from .geometric import ElasticTransform, VFlip, Shift, RotateAndScale, RotateAndScaleState, Rotate90
 from .intensity import GaussianNoise
--- metadamagenet/augment/enhance.py
+++ metadamagenet/augment/enhance.py
@@ -1,12 +1,12 @@
-import dataclasses
 from typing import Tuple
 
+import kornia.color
 import torch
 import kornia.enhance as ke
 
 from .base import Transform
 
-__all__ = ('Clahe', 'Brightness', 'Contrast', 'Saturation', 'RGBShift', 'RGBShiftState', 'HSVShift', 'HSVShiftState')
+__all__ = ('Clahe', 'Brightness', 'Contrast', 'Saturation', 'RGBShift', 'HSVShift')
 
 
 class Clahe(Transform[None]):
@@ -28,7 +28,7 @@ class Clahe(Transform[None]):
 
 class Brightness(Transform[torch.FloatTensor]):
 
-    def __init__(self, factor_range: Tuple[float, float] = (0.2, 0.8)):
+    def __init__(self, factor_range: Tuple[float, float] = (-0.1, 0.1)):
         super().__init__()
         self._factor_range: Tuple[float, float] = factor_range
 
@@ -41,7 +41,7 @@ class Brightness(Transform[torch.FloatTensor]):
 
 class Contrast(Transform[torch.FloatTensor]):
 
-    def __init__(self, factor_range: Tuple[float, float] = (0.2, 0.8)):
+    def __init__(self, factor_range: Tuple[float, float] = (0.9, 1.1)):
         super(Contrast, self).__init__()
         self._factor_range: Tuple[float, float] = factor_range
 
@@ -53,7 +53,7 @@ class Contrast(Transform[torch.FloatTensor]):
 
 
 class Saturation(Transform[torch.FloatTensor]):
-    def __init__(self, factor_range: Tuple[float, float] = (0.9, 1.1)):
+    def __init__(self, factor_range: Tuple[float, float] = (0.5, 1.5)):
         super(Saturation, self).__init__()
         self._factor_range: Tuple[float, float] = factor_range
 
@@ -64,72 +64,54 @@ class Saturation(Transform[torch.FloatTensor]):
         return ke.adjust_saturation(images, state)
 
 
-@dataclasses.dataclass
-class RGBShiftState:
-    r_shift: torch.FloatTensor
-    g_shift: torch.FloatTensor
-    b_shift: torch.FloatTensor
-
-
-class RGBShift(Transform[RGBShiftState]):
+class RGBShift(Transform[torch.FloatTensor]):
     def __init__(self,
-                 r_range: Tuple[int, int] = (-.1, .1),
-                 g_range: Tuple[int, int] = (-.1, .1),
-                 b_range: Tuple[int, int] = (-.1, .1)):
+                 r: Tuple[float, float] = (-.1, .1),
+                 g: Tuple[float, float] = (-.1, .1),
+                 b: Tuple[float, float] = (-.1, .1)):
         """
-        :param r_range: range of R channel shift
-        :param g_range: range of G channel shift
-        :param b_range: range of B channel shift
+        :param r: range of R channel shift
+        :param g: range of G channel shift
+        :param b: range of B channel shift
         """
         super().__init__()
-        self._r_range: Tuple[int, int] = r_range
-        self._g_range: Tuple[int, int] = g_range
-        self._b_range: Tuple[int, int] = b_range
+        self._r_range: Tuple[int, int] = r
+        self._g_range: Tuple[int, int] = g
+        self._b_range: Tuple[int, int] = b
 
-    def generate_state(self, input_shape: torch.Size) -> RGBShiftState:
+    def generate_state(self, input_shape: torch.Size) -> torch.FloatTensor:
         batch_size = input_shape[0]
-        return RGBShiftState(
-            r_shift=torch.rand((batch_size,)) * (self._r_range[1] - self._r_range[0]) + self._r_range[0],
-            g_shift=torch.rand((batch_size,)) * (self._g_range[1] - self._g_range[0]) + self._g_range[0],
-            b_shift=torch.rand((batch_size,)) * (self._b_range[1] - self._b_range[0]) + self._b_range[0]
-        )
+        return torch.stack(
+            (torch.rand((batch_size,)) * (self._r_range[1] - self._r_range[0]) + self._r_range[0],
+             torch.rand((batch_size,)) * (self._g_range[1] - self._g_range[0]) + self._g_range[0],
+             torch.rand((batch_size,)) * (self._b_range[1] - self._b_range[0]) + self._b_range[0]), dim=1)
 
-    def forward(self, images: torch.FloatTensor, state: RGBShiftState) -> torch.FloatTensor:
-        return ke.shift_rgb(images, state.r_shift, state.g_shift, state.b_shift)
-
-
-@dataclasses.dataclass
-class HSVShiftState:
-    h_shift: torch.FloatTensor
-    s_shift: torch.FloatTensor
-    v_shift: torch.FloatTensor
+    def forward(self, images: torch.FloatTensor, state: torch.FloatTensor) -> torch.FloatTensor:
+        return (images + state).clamp_(min=0, max=1)
 
 
-class HSVShift(Transform[HSVShiftState]):
+class HSVShift(Transform[torch.FloatTensor]):
 
     def __init__(self,
-                 h_range: Tuple[int, int] = (-.1, .1),
-                 s_range: Tuple[int, int] = (-.1, .1),
-                 v_range: Tuple[int, int] = (-.1, .1)):
+                 h: Tuple[float, float] = (-.1, .1),
+                 s: Tuple[float, float] = (-.1, .1),
+                 v: Tuple[float, float] = (-.1, .1)):
         """
-        :param h_range: range of hue channel shift
-        :param s_range: range of saturation channel shift
-        :param v_range: range of brightness/value channel shift
+        :param h: range of hue channel shift (will be multiplied by 2*pi)
+        :param s: range of saturation channel shift
+        :param v: range of brightness/value channel shift
         """
         super().__init__()
-        self._h_range: Tuple[int, int] = h_range
-        self._s_range: Tuple[int, int] = s_range
-        self._v_range: Tuple[int, int] = v_range
+        self._h_range: Tuple[int, int] = h
+        self._s_range: Tuple[int, int] = s
+        self._v_range: Tuple[int, int] = v
 
-    def generate_state(self, input_shape: torch.Size) -> HSVShiftState:
+    def generate_state(self, input_shape: torch.Size) -> torch.FloatTensor:
         batch_size = input_shape[0]
-        return HSVShiftState(
-            h_shift=torch.rand((batch_size,)) * (self._h_range[1] - self._h_range[0]) + self._h_range[0],
-            s_shift=torch.rand((batch_size,)) * (self._s_range[1] - self._s_range[0]) + self._s_range[0],
-            v_shift=torch.rand((batch_size,)) * (self._v_range[1] - self._v_range[0]) + self._v_range[0]
-        )
+        return torch.stack(
+            ((torch.rand((batch_size,)) * (self._h_range[1] - self._h_range[0]) + self._h_range[0]) * (2 * torch.pi),
+             torch.rand((batch_size,)) * (self._s_range[1] - self._s_range[0]) + self._s_range[0],
+             torch.rand((batch_size,)) * (self._v_range[1] - self._v_range[0]) + self._v_range[0]), dim=1)
 
-    def forward(self, images: torch.FloatTensor, state: HSVShiftState) -> torch.FloatTensor:
-        images = ke.adjust_hue(images, state.h_shift)
-        images = ke.adjust_saturation(images, state.s_shift)
-        return ke.adjust_brightness(images, state.v_shift)
+    def forward(self, images: torch.FloatTensor, state: torch.FloatTensor) -> torch.FloatTensor:
+        return kornia.color.hsv_to_rgb(kornia.color.rgb_to_hsv(images) + state).clamp_(min=0, max=1)
