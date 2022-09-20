9418e094c41ec7dc440574c790fa1a13ab0f0f77
nimaafshar <nima.afsharh@gmail.com>
2022-09-20 15:29:33 +0430

    change probability defenition

--- metadamagenet/augment/base.py
+++ metadamagenet/augment/base.py
@@ -11,7 +11,7 @@ def _assert_prob(prob: float) -> float:
     """
     assert if some float is a probability
     """
-    if not 0 <= prob < 1:
+    if not 0 <= prob <= 1:
         raise ValueError(f"{prob} is not a valid probability")
     return prob
 
