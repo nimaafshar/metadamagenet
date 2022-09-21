4897bb843df27d0c5f68209a5cbadcb9ad9620f4
nimaafshar <nima.afsharh@gmail.com>
2022-09-22 01:07:13 +0330

    add BestCrop

--- metadamagenet/augment/geometric.py
+++ metadamagenet/augment/geometric.py
@@ -1,13 +1,13 @@
-from typing import Tuple
+from typing import Tuple, Optional, Sequence
 import dataclasses
 
 import torch
 import kornia.geometry as kg
 
-from .base import Transform
+from .base import Transform, CollectionTransform, ImageCollection
 from .utils import random_float_tensor
 
-__all__ = ('VFlip', 'Rotate90', 'Shift', 'RotateAndScaleState', 'RotateAndScale', 'ElasticTransform')
+__all__ = ('VFlip', 'Rotate90', 'Shift', 'RotateAndScaleState', 'RotateAndScale', 'ElasticTransform', 'BestCrop')
 
 
 class VFlip(Transform[None]):
@@ -109,9 +109,63 @@ class RotateAndScale(Transform[RotateAndScaleState]):
         )
 
     def forward(self, images: torch.FloatTensor, state: RotateAndScaleState) -> torch.FloatTensor:
-        transform_mat: torch.Tensor = kg.get_rotation_matrix2d(state.center, state.angle, state.scale)
+        _, _, h, w = images.size()
+        transform_mat: torch.Tensor = kg.get_rotation_matrix2d(
+            state.center * torch.tensor([h, w], device=self.device, dtype=torch.float32),
+            state.angle,
+            state.scale)
         return kg.transform.warp_affine(images,
                                         transform_mat,
                                         dsize=(images.size(-2), images.size(-1)),
                                         mode='bilinear',
                                         padding_mode='reflection')
+
+
+class BestCrop(CollectionTransform):
+    def __init__(self,
+                 samples: int = 5,
+                 size_range: Tuple[float, float] = (0.5, 0.7),
+                 dsize: Tuple[int, int] = (512, 512),
+                 msk_key: str = 'msk',
+                 only_on: Optional[Sequence[str]] = None):
+        super().__init__()
+        self._samples: int = samples
+        self._size_range: Tuple[float, float] = size_range
+        self._dsize: Tuple[int, int] = dsize
+        self._msk_key: str = msk_key
+        self._only_on: Optional[Sequence[str]] = only_on
+
+    def _generate_boxes(self, batch_size: int, count: int) -> torch.FloatTensor:
+        sizes_rel = random_float_tensor((batch_size * count,), self._size_range, device=self.device) \
+            .unsqueeze(1).repeat(1, 2)
+        bottom_rel = sizes_rel.clone()
+        bottom_rel[:, 0] = 0
+        right_rel = sizes_rel.clone()
+        right_rel[:, 1] = 0
+
+        top_left_rel = torch.rand_like(sizes_rel) * (1 - sizes_rel)
+        top_right_rel = top_left_rel + right_rel
+        bottom_left_rel = top_left_rel + bottom_rel
+        bottom_right_rel = top_left_rel + sizes_rel
+        return torch.stack((top_left_rel, top_right_rel, bottom_right_rel, bottom_left_rel), dim=1)
+
+    def forward(self, collection: ImageCollection) -> ImageCollection:
+        b, c, h, w = collection[self._msk_key].size()
+        img_shape = torch.tensor([h, w], device=self.device)
+        repeated_msk = collection[self._msk_key].unsqueeze(1).repeat(1, self._samples, 1, 1, 1) \
+            .reshape(b * self._samples, c, h, w)
+
+        candidate_boxes: torch.FloatTensor = self._generate_boxes(b, self._samples) * img_shape
+        candidates: torch.FloatTensor = kg.transform.crop_and_resize(repeated_msk, candidate_boxes, size=self._dsize) \
+            .reshape(b, self._samples, -1)
+        indices = candidates.sum(dim=2).argmax(dim=1, keepdim=True).unsqueeze(-1).repeat(1, 1, 4 * 2)
+        boxes = candidate_boxes.reshape(b, self._samples, 4 * 2).gather(dim=1, index=indices).reshape(b, 4, 2)
+
+        result: ImageCollection = {}
+        keys = self._only_on if self._only_on is not None else collection.keys()
+        for key, val in collection.items():
+            if key in keys:
+                result[key] = kg.transform.crop_and_resize(val, boxes, size=self._dsize)
+            else:
+                result[key] = val
+        return result
