c85fadd2c79ed7ac48d8f56c499ee36496a1bda1
nimaafshar <nima.afsharh@gmail.com>
2022-09-22 01:07:13 +0330

    add BestCrop

--- metadamagenet/augment/__init__.py
+++ metadamagenet/augment/__init__.py
@@ -3,7 +3,7 @@ from .test_time import TestTimeAugmentor, FourFlips
 from .base import ImageCollection, Random, Transform, CollectionTransform, OnlyOn, OneOf
 from .enhance import Clahe, Brightness, Contrast, Saturation, RGBShift, HSVShift
 from .filter import Blur
-from .geometric import ElasticTransform, VFlip, Shift, RotateAndScale, RotateAndScaleState, Rotate90
+from .geometric import ElasticTransform, VFlip, Shift, RotateAndScale, RotateAndScaleState, Rotate90, BestCrop
 from .intensity import GaussianNoise
 from .utils import random_float_tensor
 from .transforms import Resize
