11d2b4a485b87d5f00b10b39c88c3d6a599ec2a9
nimaafshar <nima.afsharh@gmail.com>
2022-09-22 14:47:28 +0330

    temporarily disabling localization and classification dataset

--- metadamagenet/dataset/__init__.py
+++ metadamagenet/dataset/__init__.py
@@ -1,4 +1,4 @@
 from .data_time import DataTime
 from .image_data import ImageData, discover_directories, discover_directory
-from .localization import LocalizationDataset
-from .classification import ClassificationDataset
+# from .localization import LocalizationDataset
+# from .classification import ClassificationDataset
