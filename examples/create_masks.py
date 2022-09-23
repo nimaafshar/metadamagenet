d30ce1ef22220010cd7b5a72a1d516a18ef7e240
nimaafshar <nima.afsharh@gmail.com>
2022-09-23 19:01:23 +0330

    change mask creation script

--- examples/create_masks.py
+++ examples/create_masks.py
@@ -1,21 +1,31 @@
-import emoji
-import numpy as np
-
-import cv2
-import timeit
+from typing import Tuple
+import argparse
 import sys
 from multiprocessing import Pool
+import pathlib
 
+import numpy as np
+import numpy.typing as npt
+import cv2
 import shapely.geometry
+from tqdm.autonotebook import tqdm
 
-from src import configs
-from src.setup import single_thread_numpy, set_random_seeds
-from src.file_structure import Dataset, ImageData, DataTime
-from src.logs import log
+from metadamagenet.configs import DamageType, GeneralConfig
+from metadamagenet.utils import single_thread_numpy, set_random_seeds
+from metadamagenet.dataset import ImageData, DataTime, discover_directory
+from metadamagenet.logging import log
 
 single_thread_numpy()
 set_random_seeds()
 sys.setrecursionlimit(10000)
+
+damage_type_color = {
+    DamageType.UN_CLASSIFIED: 1,
+    DamageType.NO_DAMAGE: 1,
+    DamageType.MINOR_DAMAGE: 2,
+    DamageType.MAJOR_DAMAGE: 3,
+    DamageType.DESTROYED: 4
+}
 
 
 def mask_for_polygon(poly: shapely.geometry.Polygon, im_size=(1024, 1024)):
@@ -28,52 +38,57 @@ def mask_for_polygon(poly: shapely.geometry.Polygon, im_size=(1024, 1024)):
 
     img_mask = np.zeros(im_size, np.uint8)
     exteriors = [int_coords(poly.exterior.coords)]
-    interiors = [int_coords(pi.coords) for pi in poly.interiors]
+    interiors = [int_coords(p.coords) for p in poly.interiors]
     cv2.fillPoly(img_mask, exteriors, 1)
     cv2.fillPoly(img_mask, interiors, 0)
     return img_mask
 
 
-def create_masks(image_data: ImageData):
+def create_masks(image_data: ImageData) -> Tuple[npt.NDArray, npt.NDArray]:
     """
-    creates a polygon and a damage mask from image labels and saves them to `masks` folder
+    creates localization and classification mask for image data
     """
-    polygons_mask = np.zeros((1024, 1024), dtype='uint8')  # a mask-image including polygons
-    damages_mask = np.zeros((1024, 1024), dtype='uint8')  # a mask-image with damage levels
+    localization_mask = np.zeros((1024, 1024), dtype='uint8')  # a mask-image including polygons
+    classification_mask = np.zeros((1024, 1024), dtype='uint8')  # a mask-image with damage levels
 
-    for polygon, _ in image_data.polygons(DataTime.PRE):
+    for polygon in image_data.polygons(DataTime.PRE):
         _msk = mask_for_polygon(polygon)
-        polygons_mask[_msk > 0] = 1
+        localization_mask[_msk > 0] = 1
 
+    damage_type: DamageType
     for polygon, damage_type in image_data.polygons(DataTime.POST):
         _msk = mask_for_polygon(polygon)
-        damages_mask[_msk > 0] = damage_type
+        classification_mask[_msk > 0] = damage_type_color[damage_type]
+
+    return localization_mask, classification_mask
+
+
+def create_and_save_targets(image_data: ImageData) -> None:
+    localization_msk, classification_msk = create_masks(image_data)
 
     cv2.imwrite(str(image_data.mask(DataTime.PRE)),
-                polygons_mask,
+                localization_msk,
                 [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
     cv2.imwrite(str(image_data.mask(DataTime.POST)),
-                damages_mask,
+                classification_msk,
                 [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
 
 if __name__ == '__main__':
-    t0 = timeit.default_timer()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=pathlib.Path, required=True)
+    args = parser.parse_args()
+    assert args.path.exists() and args.path.is_dir(), \
+        f"path {args.path.absolute()} does not exist or its not a directory"
 
-    configs.GeneralConfig.load()
-
-    config: configs.GeneralConfig = configs.GeneralConfig.get_instance()
-
-    train_dataset = Dataset(config.train_dirs)
-    train_dataset.discover()
-
+    GeneralConfig.load()
+    config = GeneralConfig.get_instance()
     # create mask directories
-    for train_dir in config.train_dirs:
-        (train_dir / 'masks').mkdir(exist_ok=True)
+    (args.dir / config.masks_dirname).mkdir(exist_ok=True)
+    image_dataset = discover_directory(args.path)
 
     with Pool() as pool:
-        _ = pool.map(create_masks, train_dataset.images)
+        tqdm(pool.imap(create_and_save_targets, image_dataset), total=len(image_dataset))
 
-    elapsed = timeit.default_timer() - t0
-    log(':hourglass: : {:.3f} min'.format(elapsed / 60))
+    log("masks created")
