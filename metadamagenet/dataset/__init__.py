0cd886cf8d07557575c783eaa87128b435711da6
nimaafshar <nima.afsharh@gmail.com>
2022-09-24 16:55:51 +0330

    Refactor datasets, add preprocessors

--- metadamagenet/dataset/__init__.py
+++ metadamagenet/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .data_time import DataTime
 from .image_data import ImageData, discover_directories, discover_directory
 from .dataset import LocalizationDataset, ClassificationDataset
+from .preprocess import LocalizationPreprocessor, ClassificationPreprocessor
