bf75e5429f5c74779e3418deab2ec0937d40f080
nimaafshar <nima.afsharh@gmail.com>
2022-09-28 18:26:43 +0330

    add wrappers for efficientnetb4,efficientb0small

--- metadamagenet/wrapper/__init__.py
+++ metadamagenet/wrapper/__init__.py
@@ -3,4 +3,7 @@ from .dpn92 import Dpn92Wrapper, Dpn92ClassifierWrapper, Dpn92LocalizerWrapper
 from .resnet34 import Resnet34Wrapper, Resnet34LocalizerWrapper, Resnet34ClassifierWrapper
 from .senet154 import SeNet154Wrapper, SeNet154LocalizerWrapper, SeNet154ClassifierWrapper
 from .seresnext50 import SeResnext50Wrapper, SeResnext50ClassifierWrapper, SeResnext50LocalizerWrapper
-from .efficient import EfficientUnetB0Wrapper, EfficientUnetB0LocalizerWrapper
+from .efficient import (
+    EfficientUnetB0Wrapper, EfficientUnetB0LocalizerWrapper,
+    EfficientUnetB0SmallWrapper, EfficientUnetB0SmallLocalizerWrapper,
+    EfficientUnetB4Wrapper, EfficientUnetB4LocalizerWrapper)
