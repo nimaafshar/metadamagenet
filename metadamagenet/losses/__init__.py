02c90ac15b45b809a3e9b010bfe9a062d0452f0b
nimaafshar <nima.afsharh@gmail.com>
2022-10-03 02:38:12 +0330

    add focalloss2d

--- metadamagenet/losses/__init__.py
+++ metadamagenet/losses/__init__.py
@@ -1,3 +1,4 @@
 from .dice import binary_dice_with_logits, BinaryDiceLossWithLogits
 from .weighted import WeightedSum
 from .cross_entropy import SegmentationCCE
+from .focal import BinaryFocalLoss2d, FocalLoss2d
