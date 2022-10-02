7d7b7d7755ae607ac14a62b3cc93f92710bf6aa7
nimaafshar <nima.afsharh@gmail.com>
2022-10-02 16:35:50 +0330

    add new losses

--- metadamagenet/losses/weighted.py
+++ metadamagenet/losses/weighted.py
@@ -3,51 +3,30 @@ from typing import Tuple
 import torch
 from torch import nn
 
-from .base import MonitoredImageLoss, Monitored
-from ..metrics.average import AverageMetric
 
-
-class WeightedLoss(MonitoredImageLoss):
+class WeightedSum(nn.Module):
     """
     example:
-    ComboLoss(
-        ("BCE",StableBCELoss(), 1),
-        ("DICE",WithSigmoid(DiceLoss(per_image=False)), 1),
-        ("JACCARD",WithSigmoid(JaccardLoss(per_image=False)), 1),
-        ("Lovasz",LovaszLoss(per_image), 1),
-        ("Lovasz_sig",WithSigmoid(LovaszLossSigmoid(per_image)), 1),
-        ("focal",WithSigmoid(FocalLoss2d()), 1)
+    ```python
+    loss = WeightedSum(
+        (Loss1(), 1),
+        (Loss2()), 10)
     )
+
+    loss(logits,targets).backward()
+    ```
     """
 
-    def __init__(self, *weighted_losses: Tuple[str, nn.Module, float]):
+    def __init__(self, *weighted_losses: Tuple[nn.Module, float]):
         super().__init__()
         losses: Tuple[nn.Module]
         weights: Tuple[float]
-        names, losses, weights = zip(*weighted_losses)
-        self.losses: nn.ModuleList[MonitoredImageLoss] = nn.ModuleList(
-            [loss if isinstance(loss, MonitoredImageLoss) else Monitored(loss) for loss in losses]
-        )
+        losses, weights = zip(*weighted_losses)
+        self.losses: nn.ModuleList = nn.ModuleList(losses)
         self.register_buffer("weights", torch.FloatTensor(weights))
-        self._names = names
-        self._total_loss_meter: AverageMetric = AverageMetric()
 
     def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         weighted_loss: torch.Tensor = 0
         for i, loss in enumerate(self.losses):
             weighted_loss += loss(outputs, targets) * self.weights[i]
-        self._total_loss_meter.update(weighted_loss.item(), outputs.size(0))
         return weighted_loss
-
-    def till_here(self) -> float:
-        return self._total_loss_meter.till_here()
-
-    def status_till_here(self) -> str:
-        return f"Weighted: {self._total_loss_meter.status_till_here()}[" + \
-               ",".join((f"{self._names[i]}: {loss.status_till_here()}" for i, loss in enumerate(self.losses))) + \
-               "]"
-
-    def reset(self) -> None:
-        for loss in self.losses:
-            loss.reset()
-        self._total_loss_meter.reset()
