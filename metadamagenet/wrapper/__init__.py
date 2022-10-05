99622bcc5d42838dababc32ccebad3ae386e2644
nimaafshar <nima.afsharh@gmail.com>
2022-10-05 14:35:28 +0330

    fix metadata typehint

--- metadamagenet/wrapper/__init__.py
+++
@@ -1,8 +0,0 @@
-from .wrapper import ModelWrapper
-from .dpn92 import Dpn92Wrapper, Dpn92ClassifierWrapper, Dpn92LocalizerWrapper
-from .resnet34 import Resnet34Wrapper, Resnet34LocalizerWrapper, Resnet34ClassifierWrapper
-from .senet154 import SeNet154Wrapper, SeNet154LocalizerWrapper, SeNet154ClassifierWrapper
-from .seresnext50 import SeResnext50Wrapper, SeResnext50ClassifierWrapper, SeResnext50LocalizerWrapper
-from .efficient import (
-    EfficientUnetB0Wrapper, EfficientUnetB0LocalizerWrapper,
-    EfficientUnetB4Wrapper, EfficientUnetB4LocalizerWrapper)
