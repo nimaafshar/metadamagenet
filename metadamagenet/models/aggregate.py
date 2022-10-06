df17f3169aee8c6ea0e897f1433902e04e223847
nimaafshar <nima.afsharh@gmail.com>
2022-10-06 16:36:44 +0330

    add model aggregator

--- metadamagenet/models/aggregate.py
+++ metadamagenet/models/aggregate.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple
+import abc
+from typing import Dict, Tuple, ClassVar
 from typing_extensions import Self
 
 import torch
@@ -7,31 +8,46 @@ from torch import Tensor, nn
 from .base import BaseModel
 
 
-class Mean(BaseModel):
+class ModelAggregator(nn.Module, metaclass=abc.ABCMeta):
+
+    @classmethod
+    @abc.abstractmethod
+    def name(cls) -> str:
+        pass
+
+    @abc.abstractmethod
+    def preprocess(self, data: Dict[str, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+    @abc.abstractmethod
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """
+        models forward + activate + aggregate
+        :return:
+        """
+
+
+class Mean(ModelAggregator):
+    @classmethod
+    def name(cls) -> str:
+        return "ModelsMean"
+
     def __init__(self, *models: BaseModel):
         super().__init__()
         self.models: nn.ModuleList[BaseModel] = nn.ModuleList(models)
         assert len(models) > 0, "len(models) == 0"
 
-    def activate(self, outputs: Tensor) -> Tensor:
-        return self.models[0].activate(outputs)
-
     def preprocess(self, data: Dict[str, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
         return self.models[0].preprocess(data)
 
-    @classmethod
-    def name(cls) -> str:
-        return "ModelsMean"
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        forward + activate + mean
+        """
         outputs_sum: torch.Tensor = 0
         for i, model in enumerate(self.models):
-            outputs: torch.Tensor = model(x)
+            outputs: torch.Tensor = model.activate(model(x))
             if i == 0:
                 outputs_sum = torch.zeros_like(outputs, device=outputs.device)
             outputs_sum += outputs
         return outputs_sum / len(self.models)
-
-    @classmethod
-    def from_pretrained(cls, version: str, seed: int, data_parallel: bool = False) -> Self:
-        raise NotImplementedError("loading pretrained is not supported for Mean aggregator")
