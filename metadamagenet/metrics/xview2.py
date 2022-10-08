21625f67c98d5ccde4fc3f6bb90136b9c389388b
nimaafshar <nima.afsharh@gmail.com>
2022-10-09 01:56:12 +0330

    classification score added

--- metadamagenet/metrics/xview2.py
+++ metadamagenet/metrics/xview2.py
@@ -1,4 +1,5 @@
+from .damage import DamageClassificationMetric, DamageLocalizationMetric
 from torchmetrics import Dice
 
 localization_score = Dice(multiclass=False, threshold=0.5, zero_division=0, average='micro')
-classification_score = NotImplemented
+classification_score = DamageClassificationMetric() * 0.7 + DamageLocalizationMetric() * 0.3
