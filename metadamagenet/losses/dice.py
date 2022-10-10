bcb349c0c6ed07db746fe108453470ace2c0159f
nimaafshar <nima.afsharh@gmail.com>
2022-10-10 19:49:42 +0330

    add multiclass losses

--- metadamagenet/losses/dice.py
+++ metadamagenet/losses/dice.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import torch
 from torch import nn
@@ -12,7 +12,7 @@ def binary_dice_with_logits(logits: torch.Tensor, true: torch.Tensor, eps: float
     loss is mean score for class 0 and 1
 
     :param logits: (N,1,H,W) float tensor
-    :param true: (
+    :param true: (N,1,H,W) with values 0 and 1
     :param eps: epsilon for numerical stability
     :return: loss, torch.Tensor
     """
@@ -42,7 +42,43 @@ class BinaryDiceLossWithLogits(nn.Module):
         return binary_dice_with_logits(logits, outputs, self._eps)
 
 
+def dice_loss_with_logits(logits: torch.Tensor, true: torch.Tensor, class_weights: torch.Tensor, eps=1e-8):
+    """
+    :param logits: torch.Tensor of size (N,num_classes,H,W) with type float
+    :param true: torch.Tensor of size (N,1,H,W) with type long with values 0,...,C-1
+    :param class_weights: torch.Tensor of size(num_classes,)
+    :param eps: epsilon for numerical stability
+    :return: loss, torch.Tensor
+    """
+    n, num_classes, h, w = logits.size()
+    assert true.max() < num_classes and true.min() >= 0, "true values should be in 0,1,...,(num_classes-1)"
+    assert class_weights.ndim() == 1 and class_weights.size(0) == num_classes, \
+        "class_weights should be of size (num_classes,)"
+    true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+    true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+    probas = tf.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice_score = (2. * intersection / (cardinality + eps))
+    loss = 1 - dice_score
+    return torch.dot(loss, class_weights)
+
+
 class DiceLoss(nn.Module):
-    def __init__(self, class_weights: List[float]):
+    def __init__(self, num_classes: int, class_weights: Optional[List[float]], eps: float = 1e-8):
         super().__init__()
-        raise NotImplementedError()
+        assert num_classes > 1, "Non Binary Dice Loss is used with num_classes>0"
+        self._num_classes: int = num_classes
+        if class_weights is None:
+            self._class_weights: torch.Tensor = torch.ones(num_classes)
+        else:
+            assert len(class_weights) == num_classes, "len(class_weights) should be equal to num_classes"
+            self._class_weights: torch.Tensor = torch.tensor(class_weights, dtype=torch.float)
+        self._eps: float = eps
+
+    def forward(self, logits: torch.Tensor, outputs: torch.Tensor):
+        return dice_loss_with_logits(logits, outputs,
+                                     class_weights=self._class_weights.to(device=logits.device),
+                                     eps=self._eps)
