1554c7331dee0198a46cc5c58e2386cf8578baa8
nimaafshar <nima.afsharh@gmail.com>
2022-10-13 16:14:52 +0330

    segmentation cce corrected

--- metadamagenet/losses/cross_entropy.py
+++ metadamagenet/losses/cross_entropy.py
@@ -10,4 +10,9 @@ class SegmentationCCE(nn.Module):
         self.ce: nn.CrossEntropyLoss = nn.CrossEntropyLoss(weights)
 
     def forward(self, outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        return self.ce(outputs, targets.argmax(dim=1))
+        """
+        :param outputs: torch.Tensor of type float and shape (N,C,H,W)
+        :param targets: torch.Tensor of type int and shape (N,1,H,W) containing values [0,C)
+        :return:
+        """
+        return self.ce(outputs, targets.squeeze(dim=1))
