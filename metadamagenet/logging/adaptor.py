b435426be2aac6c51e6667fabb6d90e105fd8cc7
nimaafshar <nima.afsharh@gmail.com>
2022-10-15 20:48:40 +0330

    restore kwargs

--- metadamagenet/logging/adaptor.py
+++ metadamagenet/logging/adaptor.py
@@ -10,5 +10,5 @@ class EmojiAdapter(logging.LoggerAdapter):
     def __init__(self, logger):
         super().__init__(logger, None)
 
-    def process(self, msg: Any, **kwargs) -> tuple[Any, MutableMapping[str, Any]]:
+    def process(self, msg: Any, kwargs: MutableMapping[str, Any]) -> tuple[Any, MutableMapping[str, Any]]:
         return emoji.emojize(msg, language='alias'), kwargs
