e0b53bfe2305ba82d098cf7786863b7422dd6368
nimaafshar <nima.afsharh@gmail.com>
2022-10-15 04:44:40 +0330

    add focalloss2d temp

--- metadamagenet/losses/__init__.py
+++ metadamagenet/losses/__init__.py
@@ -1,3 +1,3 @@
 from .dice import BinaryDiceLoss, DiceLoss
-from .focal import BinaryFocalLoss, FocalLoss
+from .focal import BinaryFocalLoss, FocalLoss, FocalLoss2d
 from .weighted import WeightedSum
--- metadamagenet/losses/focal.py
+++ metadamagenet/losses/focal.py
@@ -93,3 +93,22 @@ class FocalLoss(nn.Module):
         pt = (1 - targets) * (1 - outputs) + targets * outputs
         losses = torch.mean(-(1. - pt) ** self._gamma * torch.log(pt), dim=dims)
         return torch.dot(losses, self.class_weights.to(device=losses.device))
+
+
+class FocalLoss2d(nn.Module):
+    def __init__(self, gamma=2, ignore_index=255):
+        super().__init__()
+        self.gamma = gamma
+        self.ignore_index = ignore_index
+
+    def forward(self, outputs, targets):
+        outputs = outputs.contiguous()
+        targets = targets.contiguous()
+        eps = 1e-8
+        non_ignored = targets.view(-1) != self.ignore_index
+        targets = targets.view(-1)[non_ignored].float()
+        outputs = outputs.contiguous().view(-1)[non_ignored]
+        outputs = torch.clamp(outputs, eps, 1. - eps)
+        targets = torch.clamp(targets, eps, 1. - eps)
+        pt = (1 - targets) * (1 - outputs) + targets * outputs
+        return (-(1. - pt) ** self.gamma * torch.log(pt)).mean()
