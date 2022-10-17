cc64051d161d8f4c9ac29edd9699be4420364dd3
nimaafshar <nima.afsharh@gmail.com>
2022-10-17 17:34:55 +0330

    manual seed for all devices

--- metadamagenet/utils/setup.py
+++ metadamagenet/utils/setup.py
@@ -1,8 +1,9 @@
 import random
 import torch
 
+
 def set_random_seeds(seed: int):
     random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
