94a467522bc00c22795c92fb1e37289aa74acb83
nimaafshar <nima.afsharh@gmail.com>
2022-10-19 04:24:59 +0330

    refactor code

--- metadamagenet/losses/focal.py
+++ metadamagenet/losses/focal.py
@@ -6,6 +6,7 @@ class BinaryFocalLoss(BinaryFocalLossWithLogits):
     def forward(self, inputs: Tensor, targets: Tensor) -> Tensor:
         return super(BinaryFocalLoss, self).forward(inputs.squeeze(1), targets)
 
+
 """
 Focal Loss is removed because of numerical instability in fp16 training
 use kornia.losses.focal
